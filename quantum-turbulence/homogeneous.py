Fix a bug in the 3D homogeneous code, but find that the integration of the gap equaiton in 3D is finite, some thing wrong with the formula or the code. Need to talk to Dr. Forbes tomorrow.

quantum-turbulence/homogeneous.py
@@ -159,7 +159,8 @@ class Homogeneous1D(object):
         E = np.sqrt(e_p**2 + abs(delta)**2)
         w_p, w_m = e_m + E, e_m - E
         args = dict(locals())
-        return self.Results(*[args[_n] for _n in self.Results._fields])
+        rets = self.Results(*[args[_n] for _n in self.Results._fields])
+        return rets
 
     def get_BCS_v_n_e(self, delta, mus_eff):
         """Return `(v_0, n, mu, e)` for the 1D BCS solution at T=0."""
@@ -206,7 +207,6 @@ class Homogeneous3D(object):
             return 1./(1+np.exp(E/self.T))
 
     def get_es(self, kz,kp, mus_eff):
-        kp=0.0
         return ((kz**2+kp**2)/2.0/m - mus_eff[0],
                (kz**2+kp**2)/2.0/m - mus_eff[1])
 
@@ -217,7 +217,9 @@ class Homogeneous3D(object):
         e_p, e_m = (e_a + e_b)/2, (e_a - e_b)/2
         E = np.sqrt(e_p**2 + abs(delta)**2)
         w_p, w_m = e_m + E, e_m - E
-        return self.Results(*[locals()[_n] for _n in self.Results._fields])
+        args = dict(locals())
+        rets = self.Results(*[args[_n] for _n in self.Results._fields])
+        return rets
 
     def get_BCS_v_n_e(self, delta, mus_eff):
         """Return `(v_0, n, mu, e)` for the 1D BCS solution at T=0."""
@@ -226,8 +228,8 @@ class Homogeneous3D(object):
         def gap_integrand(kz_,kp_): #this integration will divergnce?
             res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta)
             return (1 - self.f(res.w_p) - self.f(-res.w_m))/res.E
-
-        v_0 = 4*np.pi / dquad(gap_integrand, kF,np.inf) #without regularization, v_0 should be zero?
+        gap_int = dquad(gap_integrand, kF,np.inf)# bad, the result is finite, something goes wrong
+        v_0 = 4*np.pi / gap_int #without regularization, v_0 should be zero?
 
         def np_integrand(kz_,kp_):
             """Density"""

quantum-turbulence/test_homogeneous.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.optimize import brentq
 
 import homogeneous
+from homogeneous import Homogeneous3D
 
 def test_BCS_1D(lam_inv=0.5):
     """Test a few values from Table I of Quick:1993."""
@@ -37,18 +38,14 @@ def test_Homogeneous1D_T0():
         assert np.allclose(res0.mu, res1.mus)    
 
 
-def test_Homogeneous1D_Regulization():
+def test_Homogeneous3D():
     """Compare the Homogeneous1D class with get_BCS_v_n_e for T=0."""
-    np.random.seed(2)    
-    mu_eff, delta = np.random.random(2)
-    res0 = homogeneous.get_BCS_v_n_e(mu_eff=mu_eff, delta=delta)
-    for T in [0, 0.001]:
-        res1 = homogeneous.Homogeneous1D(T=T).get_BCS_v_n_e(
-            mus_eff=(mu_eff,)*2, delta=delta)
-        assert np.allclose(res0.v_0, res1.v_0)
-        assert np.allclose(res0.n, res1.ns.sum())
-        assert np.allclose(res0.mu, res1.mus)   
+    res0 = homogeneous.get_BCS_v_n_e(mu_eff=1.2, delta=3.4)
+
+    h3 = Homogeneous3D(T=10.0)
+    res1 = h3.get_BCS_v_n_e(mus_eff=(1.2,)*2, delta=3.4)
+    (res0, res1)
 
  # to debug in Visual Studio
 if __name__ == '__main__':
-    test_Homogeneous1D_T0()
+    test_Homogeneous3D()