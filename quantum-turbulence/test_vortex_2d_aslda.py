c16503b18ef658cdb9ae42002270452d07acbb55
Chunde <xmuhcd@msn.com>
2019-01-08 17:09:53 -0800

    WIP: Implement average over k_z

--- quantum-turbulence/test_vortex_2d_aslda.py
+++ quantum-turbulence/test_vortex_2d_aslda.py
@@ -31,25 +31,39 @@ def test_derivative():
 
         # test _get_dD_dp
         dD_p =   (a._Dp(p + dx) - a._Dp(p-dx))/ 2 / dx
-        dD_p_ = a._get_dD_dp(p=p)
+        dD_p_ = a._dD_dp(p=p)
         assert np.allclose(dD_p, dD_p_, atol=0.0005)
 
         # test get_dD_dn
         dD_n_a = (a._D(na + dx,nb)-a._D(na-dx,nb))/2/dx
         dD_n_b = (a._D(na,nb + dx)-a._D(na,nb - dx))/2/dx
-        dD_n_a_, dD_n_b_ = a._get_dD_dn(ns)
+        dD_n_a_, dD_n_b_ = a._dD_dn(ns)
 
         assert np.allclose(dD_n_a, dD_n_a_, atol=0.0005)
         assert np.allclose(dD_n_b, dD_n_b_, atol=0.0005)
 
+        # test _dC_dn
+        dC_n_a = (a._C(na + dx,nb)-a._C(na-dx,nb))/2/dx
+        dC_n_b = (a._C(na,nb + dx)-a._C(na,nb - dx))/2/dx
+        dC_n_a_, dC_n_b_ = a._dC_dn(ns)
+        assert np.allclose(dC_n_a, dC_n_a_, atol=0.0005)
+        assert np.allclose(dC_n_b, dC_n_b_, atol=0.0005)
 
+        # test _dalpha_m_dp
+        dalpha_m_dp = (a._alpha_m(p + dx) - a._alpha_m(p-dx))/2/dx
+        dalpha_m_dp_ = a._dalpha_m_dp(p)
+        dalpha_p_dp = (a._alpha_p(p + dx) - a._alpha_p(p-dx))/2/dx
+        dalpha_p_dp_ = a._dalpha_p_dp(p)
+        assert np.allclose(dalpha_m_dp,dalpha_m_dp_)
+        assert np.allclose(dalpha_p_dp,dalpha_p_dp_)
 
 def test_iterate_ASLDA():
     def iterate(lda, mudelta,   na_avg=0.5, nb_avg=0.5, N_twist=0, **kw):
         mu_a, mu_b, mu_a_eff, mu_b_eff, delta,taus = mudelta
         mus = (mu_a_eff, mu_b_eff)
         if np.isinf(N_twist):
-            R = lda.get_R_twist_average(mus=mus, delta=delta, **kw)
+            #R = lda.get_R_twist_average(mus=mus, delta=delta, **kw)
+            R = lda.get_R_per_average(mus=mus, delta=delta, **kw)
         else:
             R = lda.get_R(mus=mus, delta=delta, N_twist=N_twist)
 
@@ -80,21 +94,21 @@ def test_iterate_ASLDA():
     k_F = np.sqrt(2*m*e_F)
     n_F = k_F**3/3/np.pi**2
     E_FG = 2./3*n_F*e_F
-    mu = 0.59060550703283853378393810185221521748413488992993*e_F *5#I change this number
-    delta = 0.68640205206984016444108204356564421137062514068346*e_F * 1.3
-    grid_size = 32
+    mu = 0.59060550703283853378393810185221521748413488992993*e_F #I change this number
+    delta = 0.68640205206984016444108204356564421137062514068346*e_F
+    grid_size = 16
     aslda = vortex_2d_aslda.ASLDA(Nxy=(grid_size,)*2)
     k_c = abs(aslda.kxy[0]).max()
     E_c = (aslda.hbar*k_c)**2/2/aslda.m
     aslda = vortex_2d_aslda.ASLDA(Nxy=(grid_size,)*2, Lxy=(0.4,)*2, E_c=E_c)
-    qT = (mu, mu) + (mu_eff*np.ones(aslda.Nxy),)*2 + (delta * np.ones((aslda.Nxy),), None)
+    qT = (mu, mu) + (mu_eff*np.ones(aslda.Nxy),)*2 + (0 * np.ones((aslda.Nxy),), None)
     #qT = (mu, mu) + (mu_eff*np.ones((np.prod(aslda.Nxy),np.prod(aslda.Nxy))),)*2 + (delta * np.ones(((np.prod(aslda.Nxy),np.prod(aslda.Nxy))),), None)
     max_iteration = 5
     v_0, n, mu, e_0 = homogeneous.get_BCS_v_n_e(delta=delta, mu_eff=mu_eff)
 
     while max_iteration > 0:
        # max_iteration -= 1
-        qT = iterate(lda=aslda,mudelta = qT, N_twist=1,na_avg=0.4 * n, nb_avg=0.6 * n, abs_tol=1e-2)
+        qT = iterate(lda=aslda,mudelta = qT, N_twist=np.inf,na_avg=0.5 * n, nb_avg=0.5 * n, abs_tol=1e-2)
 
 
 
@@ -117,7 +131,6 @@ def test_aslda():
     E_c = (s.hbar*k_c)**2/2/s.m
     s = vortex_2d_aslda.ASLDA(Nxy=(16,)*2, E_c=E_c)
     kw = dict(mus=(mu, mu), delta=delta)
-    #R = s.get_R(**kw)
     H = s.get_H(**kw)
     while(True):
         ns, taus,nu = s.get_ns_taus_nu(H)
@@ -128,4 +141,5 @@ def test_aslda():
 
 if __name__ == '__main__':
     #test_aslda()
-    test_derivative()
+    #test_derivative()
+    test_iterate_ASLDA()
--- quantum-turbulence/vortex_2d_aslda.py
+++ quantum-turbulence/vortex_2d_aslda.py
@@ -87,14 +87,29 @@ class ASLDA(object):
     def _alpha_b(self,n_a,n_b):
         return _alpha(-self._get_p((n_a,n_b)))
 
-    def _alpha_p(self,n_a,n_b):
-        return 0.5 * (self._alpha_a(n_a,n_b) + self._alpha_b(n_a,n_b))
+    def _alpha_p(self,p):
+        return 0.5 * (self._alpha(p) + self._alpha(-p))
 
-    def _alpha_m(self,n_a,n_b):
-        return 0.5 * (self._alpha_a(n_a,n_b) - self._alpha_b(n_a,n_b))
+    def _alpha_m(self,p):
+        return 0.5 * (self._alpha(p) - self._alpha(-p))
+
+    def _dalpha_p_dp(self,p):
+        """return dalpha_p / dp"""
+        "[Matlab verified]"
+        "[Numerical Test Status:Pass]"
+        return 1.064*p*(p**2 - 1.0)**2
+
+    def _dalpha_m_dp(self,p):
+        """return dalpha_m / dp"""
+        "[Matlab verified]"
+        "[Numerical Test Status:Pass]"
+        return 0.156*(p**2 - 1.0)**2
 
     def _C(self,n_a,n_b):
-        return self.alpha_p(n_a,n_b) * (n_a + n_b)**(1.0/3) / self._gamma(self._get_p(n_a,n_b))
+        """return C tilde"""
+        "[Numerical Test Status:Pass]"
+        p = self._get_p((n_a,n_b))
+        return self._alpha_p(p) * (n_a + n_b)**(1.0/3) / self._gamma(p)
 
     def _D(self,n_a,n_b):
         "[Numerical Test Status:Pass]"
@@ -107,7 +122,7 @@ class ASLDA(object):
         "[Numerical Test Status:Pass]"
         return self._G(p) - self._alpha(p) * ((1+p)/2.0)**(5.0/3) - self._alpha(-p) * ((1-p)/2.0)**(5/3)
 
-    def _get_dD_dp(self,p):
+    def _dD_dp(self,p):
         """return the derivative 'dD(p)/dp' """
         "[Numerical Test Status:Pass]"
         p_p, p_m = (1+p)**(2.0/3), (1-p)**(2.0/3)
@@ -124,22 +139,11 @@ class ASLDA(object):
         dD_p = 1.284*p + (- 0.57904  - 0.51615*p2 + 0.90042*p4 -0.42823*p6)*pm +(0.82315*p3- 0.46617 *p- 0.40065*p5) * pp
         return dD_p
 
-    def get_alphas_p(self,ns = None):
-        p = self._get_p(ns)
-        p2 = p**2
-        p4 = p2**2
-        # too make code faster, equation (98) is divived into even part and odd part
-        alpha_even,alpha_odd = 1.0094 + 0.532 * p2 * (1 - p2 + p4 / 3.0), 0.156 * p * (1 - 2.0 * p2 / 3.0 + p4 / 5.0)
-
-        alpha_a,alpha_b =  alpha_odd + alpha_even, -alpha_odd + alpha_even #fixed an error here
-        alpha_p = alpha_even # it's defined as (alpha_a + alpha_b) /2 , which is just alpha_even
-        return (alpha_a,alpha_b,alpha_p,p)
-    def _get_dD_dn(self,ns):
+    def _dD_dn(self,ns):
         """Return the derivative `dD(n_a,n_b)/d n_a and d n_b` """
         "[Numerical Test Status:Pass]"
         na, nb = ns
         n = na + nb
-
         ### matlab gives long formulas used to check numerical code.
         # the follow 3 lines is from matlab
         #nb32 = (nb/(na + nb))**(2/3)
@@ -147,21 +151,45 @@ class ASLDA(object):
         #dD_n_a = 7.5963331205759943604501182783546*n**(2/3)*((0.642*(na - 1.0*nb)**2)/n**2 - (0.000066666666666666666666666666666667*(na/n)**(5/3)*(19049.0*na**6 + 114294.0*na**5*nb + 285735.0*na**4*nb**2 + 185780.0*na**3*nb**3 + 248295.0*na**2*nb**4 + 99318.0*na*nb**5 + 16553.0*nb**6))/n**6 - (0.000066666666666666666666666666666667*(nb/n)**(5/3)*(16553.0*na**6 + 99318.0*na**5*nb + 248295.0*na**4*nb**2 + 185780.0*na**3*nb**3 + 285735.0*na**2*nb**4 + 114294.0*na*nb**5 + 19049.0*nb**6))/n**6 + 0.357) - (0.00050642220803839962403000788522364*(92448.0*na*nb**6 - 23112.0*na**6*nb + 23112.0*nb**7 + 16553.0*nb**7*na32 - 19049.0*nb**7*nb32 + 115560.0*na**2*nb**5 - 115560.0*na**4*nb**3 - 92448.0*na**5*nb**2 + 99318.0*na*nb**6*na32 + 19049.0*na**6*nb*na32 - 114294.0*na*nb**6*nb32 - 16553.0*na**6*nb*nb32 + 248295.0*na**2*nb**5*na32 - 75724.0*na**3*nb**4*na32 + 637095.0*na**4*nb**3*na32 + 114294.0*na**5*nb**2*na32 - 637095.0*na**2*nb**5*nb32 + 75724.0*na**3*nb**4*nb32 - 248295.0*na**4*nb**3*nb32 - 99318.0*na**5*nb**2*nb32))/n**(19/3)
         #dD_n_b = (0.00050642220803839962403000788522364*(23112.0*na*nb**6 - 92448.0*na**6*nb - 23112.0*na**7 + 19049.0*na**7*na32 - 16553.0*na**7*nb32 + 92448.0*na**2*nb**5 + 115560.0*na**3*nb**4 - 115560.0*na**5*nb**2 + 16553.0*na*nb**6*na32 + 114294.0*na**6*nb*na32 - 19049.0*na*nb**6*nb32 - 99318.0*na**6*nb*nb32 + 99318.0*na**2*nb**5*na32 + 248295.0*na**3*nb**4*na32 - 75724.0*na**4*nb**3*na32 + 637095.0*na**5*nb**2*na32 - 114294.0*na**2*nb**5*nb32 - 637095.0*na**3*nb**4*nb32 + 75724.0*na**4*nb**3*nb32 - 248295.0*na**5*nb**2*nb32))/n**(19/3) + 7.5963331205759943604501182783546*n**(2/3)*((0.642*(na - 1.0*nb)**2)/n**2 - (0.000066666666666666666666666666666667*(na/n)**(5/3)*(19049.0*na**6 + 114294.0*na**5*nb + 285735.0*na**4*nb**2 + 185780.0*na**3*nb**3 + 248295.0*na**2*nb**4 + 99318.0*na*nb**5 + 16553.0*nb**6))/n**6 - (0.000066666666666666666666666666666667*(nb/n)**(5/3)*(16553.0*na**6 + 99318.0*na**5*nb + 248295.0*na**4*nb**2 + 185780.0*na**3*nb**3 + 285735.0*na**2*nb**4 + 114294.0*na*nb**5 + 19049.0*nb**6))/n**6 + 0.357)
 
-
         n2 = n**2
         p = self._get_p(ns)
         p2 = p**2
-
         dp_n_a = 2*nb/n2 # dp/dna
         dp_n_b = -2*na/n2# dp/dnb
         
-        dD_p = self._get_dD_dp(p=p)
+        dD_p = self._dD_dp(p=p)
         N0 = (6 * np.pi**2) ** (5.0/3) / 20 / np.pi**2
         N1 = self._D(na,nb) / 0.6 / n
         N2 = N0 * n**(5/3) * dD_p
         dD_n_a = N1 + N2 * dp_n_a
         dD_n_b = N1 + N2 * dp_n_b
         return (dD_n_a,dD_n_b)
+
+    def _dC_dn(self,ns):
+        """return dC / dn"""
+        "[Numerical Test Status:Pass]"
+        na, nb = ns
+        n = na + nb
+        p = self._get_p(ns)
+        n2 = n**2
+        p2 = p**2
+        dp_n_a = 2*nb/n2 # dp/dna
+        dp_n_b = -2*na/n2# dp/dnb
+        dC_dn_a = self._alpha_p(p) * n **(-2/3)/3 + n**(1/3)*self._dalpha_p_dp(p) * dp_n_a
+        dC_dn_b = self._alpha_p(p) * n **(-2/3)/3 + n**(1/3)*self._dalpha_p_dp(p) * dp_n_b
+        gamma = self._gamma(p) # do not forget the gamma in the demonimor
+
+        return (dC_dn_a/gamma,dC_dn_b/gamma)
+
+    def get_alphas(self,ns = None):
+        p = self._get_p(ns)
+        p2 = p**2
+        p4 = p2**2
+        # too make code faster, equation (98) is divived into even part and odd part
+        alpha_even,alpha_odd = 1.0094 + 0.532 * p2 * (1 - p2 + p4 / 3.0), 0.156 * p * (1 - 2.0 * p2 / 3.0 + p4 / 5.0)
+        alpha_a,alpha_b =  alpha_odd + alpha_even, -alpha_odd + alpha_even #fixed an error here
+        alpha_p = alpha_even # it's defined as (alpha_a + alpha_b) /2 , which is just alpha_even
+        return (alpha_a,alpha_b,alpha_p)
 
     def get_nabla(self, twist=(0,0)):
         k_bloch = np.divide(twist, self.Lxy)
@@ -204,37 +232,44 @@ class ASLDA(object):
         K_b = - self.hbar**2/2/self.m * self.get_modified_K(D2,alpha_b)
         return (K_a,K_b)
 
-    def get_modified_Vs(self,delta, ns=None, taus=None,nu=0, alphas = None,twist=(0,0)):
+    def get_modified_Vs(self,delta, ns=None, taus=None, nu=0, alphas = None,twist=(0,0)):
         """get the modified V functional terms"""
-        return self.v_ext
+       # return self.v_ext
         if ns == None or taus == None or alphas == None:
             return self.v_ext
         U_a, U_b = self.v_ext
         k_bloch = np.divide(twist, self.Lxy)
         kxy = [_k + _kb for _k, _kb in zip(self.kxy, k_bloch)]
         if(alphas == None):
-            alphas = self.get_alphas_p(ns)
+            alphas = self.get_alphas(ns)
+
         tau_a, tau_b = taus
         tau_p, tau_m = tau_a + tau_b,tau_a - tau_b
         na,nb = ns
         n = na + nb
         n2 = n**2
-        alpha_a, alpha_b, alpha_p,p = alphas
-        p2,p3,p4,p5,p6 = p**2,p**3,p**4,p**5, p**6
-        # alpha_p = 1.094 - 0.532*p2 + 0.532*p4 - 0.177333*p6
-        alpha_m = 0.156*p + 0.104*p3 + 0.0312*p5
+        alpha_a, alpha_b, alpha_p = alphas
+        p = (na-nb)/n
+        #p2,p3,p4,p5,p6 = p**2,p**3,p**4,p**5, p**6
+        #alpha_m = 0.156*p + 0.104*p3 + 0.0312*p5
         dp_n_a = 2*nb/n2
         dp_n_b = -2*na/n2
-        dalpha = -1.064*p5 + 0.156*p4 + 2.128*p3 + 0.312*p2-1.064*p+0.156
-        dalpha_p = -1.064*p5 + 2.128*p3 - 1.064*p
-        dalpha_m = 0.156*(p2+1.)**2
-        dalpha_p_n_a, dalpha_p_n_b, dalpha_m_a, dalpha_m_b= dalpha_p * dp_n_a, dalpha_p * dp_n_b, dalpha_m * dp_n_a, dalpha_m * dp_n_b # apply chain rule
-        # partial C / partial a and b
-        dC_n_a,dC_n_b = (dalpha_p * dp_n_a + alpha_p * n **(-2/3) / 3)/self.gamma,(dalpha_p * dp_n_b + alpha_p * n **(-2/3) / 3)/self.gamma # comman term can be compute just once, do it later
-        dD_n_a,dD_n_b = self._get_dD_dn(ns=ns)
+        dalpha_p = self._dalpha_p_dp(p)
+        dalpha_m = self._dalpha_m_dp(p)
+        dalpha_p_dn_a, dalpha_p_dn_b, dalpha_m_dn_a, dalpha_m_dn_b= dalpha_p * dp_n_a, dalpha_p * dp_n_b, dalpha_m * dp_n_a, dalpha_m * dp_n_b # apply chain rule
+        #dalpha = -1.064*p5 + 0.156*p4 + 2.128*p3 + 0.312*p2-1.064*p+0.156
+        #dalpha_p = -1.064*p5 + 2.128*p3 - 1.064*p
+        #dalpha_m = 0.156*(p2+1.)**2 # matlab give a different result, it's minus sign in the parensis not plus, matlab is right, this result from mathematica is wrong
+        # partial C / partial a and b, and this line is right as it yield same results as _Dc_dn
+        #dC_n_a,dC_n_b = (dalpha_p * dp_n_a + alpha_p * n **(-2/3) / 3)/self.gamma,(dalpha_p * dp_n_b + alpha_p * n **(-2/3) / 3)/self.gamma # comman term can be compute just once, do it later
+        #dC_dn_a_, dC_dn_b_ = self._dC_dn(ns)
+        #assert np.allclose(dC_n_a, dC_dn_a_, atol=0.0005)
+        #assert np.allclose(dC_n_b, dC_dn_b_, atol=0.0005)
+        dC_dn_a, dC_dn_b = self._dC_dn(ns)
+        dD_dn_a,dD_dn_b = self._dD_dn(ns=ns)
         C1 = self.hbar**2 /2/self.m 
-        V_a = dalpha_m_a * tau_m * C1  + dalpha_p_n_a * (tau_p * C1 - delta.conj().T * nu / alpha_p) + dC_n_a + dD_n_a + U_a # the common term can be compute just once
-        V_b = dalpha_m_b * tau_m * C1 + dalpha_p_n_b * (tau_p * C1 - delta.conj().T * nu / alpha_p) + dC_n_b + dD_n_b + U_b
+        V_a = dalpha_m_dn_a * tau_m * C1  + dalpha_p_dn_a * (tau_p * C1 - delta.conj().T * nu / alpha_p) + dC_dn_a + dD_dn_a + U_a # the common term can be compute just once
+        V_b = dalpha_m_dn_b * tau_m * C1 + dalpha_p_dn_b * (tau_p * C1 - delta.conj().T * nu / alpha_p) + dC_dn_b + dD_dn_b + U_b
         return (V_a,V_b)
 
     def get_Lambda(self,k0,kc,dim = 2):
@@ -258,13 +293,13 @@ class ASLDA(object):
         g = alpha_p/(C - Lambda)
         return g
 
-    def get_Ks_Vs(self, delta, mus=(0,0), ns=None, taus=None, nu=0, twist=(0, 0)): 
+    def get_Ks_Vs(self, delta, mus=(0,0), ns=None, taus=None, nu=0, kz=0, twist=(0, 0)): 
         """Return the kinetic energy and modifled potential matrics."""
-        alphas = self.get_alphas_p(ns)
+        alphas = self.get_alphas(ns)
         if alphas == None or ns == None or taus == None:
             return (self.get_Ks(twist=twist), self.get_modified_Vs(delta=delta,ns=ns,taus=taus,nu=nu,alphas=alphas,twist=twist))
 
-        alpha_a, alpha_b, alpha_p,p = alphas
+        alpha_a, alpha_b, alpha_p = alphas
 
         if type(alpha_a) != type(np.array):
             alpha_a = np.eye(self.Nxy[0]) * alpha_a # this will be problematic if Nx != Ny
@@ -284,14 +319,10 @@ class ASLDA(object):
 
     def get_ns_taus_nu(self, H, Ec=0): # Ec not used yet
         """Return the n_a, n_b"""
-        #print(H[:np.prod(self.Nxy), np.prod(self.Nxy):].real)
         Nx, Ny = self.Nxy
-        # Es and psi contain redudant information, we may only need the first half with eith nagetive or positive energy
         Es, psi = np.linalg.eigh(H) 
-        # after some debugging, fix the error when doing matrix manipulations.
         us, vs = psi.reshape(2, Nx*Ny , Nx*Ny*2)
         us,vs = us.T,vs.T
-        #print(vs[0].real)
         # density
         n_a, n_b = np.sum(np.abs(us[i])**2 * self.f(Es[i])  for i in range(len(us))).reshape(self.Nxy)/self.dx**2, np.sum(np.abs(vs[i])**2 * self.f(-Es[i])  for i in range(len(vs))).reshape(self.Nxy)/self.dx**2
         #Tau terms
@@ -311,7 +342,7 @@ class ASLDA(object):
             f = (1 - np.sign(E))/2
         return np.where(abs(E)<E_c, f, 0)
 
-    def get_H(self, mus, delta, ns=None,taus=None, nu=0, twist=(0,0)):
+    def get_H(self, mus, delta, ns=None,taus=None, nu=0,twist=(0,0)):
         """Return the single-particle Hamiltonian with pairing. """
         zero = np.zeros_like(sum((np.prod(self.Nxy),np.prod(self.Nxy))))
         Delta = np.diag((delta.reshape(self.Nxy) + zero).ravel()) # I do not understand why we just take the diagnal term of Delta?
@@ -325,34 +356,42 @@ class ASLDA(object):
         assert np.allclose(H,H.conj().T)
         return np.asarray(H)
 
-    def get_R(self, mus, delta, N_twist=1, twists=None):
+    def get_R(self, mus, delta, N_twist=1, kz=0,twists=None):
         """Return the density matrix R."""
         N = self.Nxy
         Rs = []
         if twists is None:
-            twists = itertools.product(
-                (np.arange(0, N_twist)*2*np.pi/N_twist, )*2)
+            twists = itertools.product(*(np.arange(0, N_twist)*2*np.pi/N_twist, )*2)
 
         for twist in twists:
             H = self.get_H(mus=mus, delta=delta, twist=twist)
             d, UV = np.linalg.eigh(H)
-            R = UV.dot(self.f(d)[:, None]*UV.conj().T)
+            R = UV.dot(self.f(d + (self.hbar*kz)**2/2/self.m)[:, None]*UV.conj().T)
             # R_ = np.eye(2*N) - UV.dot(self.f(-d)[:, None]*UV.conj().T)
             # assert np.allclose(R, R_)
             Rs.append(R)
         R = sum(Rs)/len(Rs)
         return R
 
-    def get_R_twist_average(self, mus, delta, abs_tol=1e-12):
+    def get_R_twist_average(self, mus, delta, kz=0,abs_tol=1e-12):
         """Return the density matrix R."""
         R0 = 1.0
         def f(twist):
             H = self.get_H(mus=mus, delta=delta, twist=twist)
             d, UV = np.linalg.eigh(H)
-            R = UV.dot(self.f(d)[:, None]*UV.conj().T)
+            R = UV.dot(self.f(d + (self.hbar*kz)**2/2/self.m)[:, None]*UV.conj().T)
             return R/R0
         R0 = f(0)
         R = R0 * mquad(f, -np.pi, np.pi, abs_tol=abs_tol)/2/np.pi
+        return R
+
+    def get_R_per_average(self,mus,delta, abs_tol=1e-12):
+        
+        def f(kz):
+            R = self.get_R(mus=mus,delta=delta,N_twist=1,kz=kz)
+            return R
+        kc = np.sqrt(2 * self.m * self.E_c)/self.hbar
+        R = mquad(f,-kc,kc,abs_tol=abs_tol) # may need to divide a factor of 2*kc?
         return R
 
     def get_LDA(mu_eff, delta):
