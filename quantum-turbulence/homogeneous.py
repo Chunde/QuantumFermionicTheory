baf005920ca99715b0b79408f68fd88c1ba28138
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-01-23 18:54:13 -0800

    DOC: Updated notes on Fulde Ferrell state after discussion with Chunde.

--- quantum-turbulence/homogeneous.py
+++ quantum-turbulence/homogeneous.py
@@ -24,25 +24,37 @@ def quad(f, kF=None, k_0=0, k_inf=np.inf, limit=1000):
         warnings.warn("Gap integral did not converge: res, err = %g, %g" % (res, err))
     return 2*res   # Accounts for integral from -inf to inf
 
+
 def dquad(f, kF=None, k_0=0, k_inf=np.inf, limit=1000, int_name="Gap"):
     """Wrapper for quad that deals with singularities
     at the Fermi surface.
     """
+    print(f"Computing {int_name}...")
     if kF is None:
-        res, err = sp.integrate.dblquad(f, k_0, k_inf, lambda x: k_0, lambda x: k_inf)
+        res, err = sp.integrate.dblquad(f, k_0, k_inf,
+                                        lambda x: k_0, lambda x: k_inf)
     else:
         # One might think that `points=[kF]` could be used here, but
         # this does not work with infinite limits.
-        res0, err0 = sp.integrate.dblquad(f, k_0, kF, lambda x: k_0, lambda x: k_inf)
-        #res1, err1 = sp.integrate.dblquad(f, kF, np.inf, lambda x: kF, lambda x: k_inf) # Chunde made a very stupid mistake, in this line, I integrate in y direction from KF to infinity, it should be from zero to infinity
-        res1, err1 = sp.integrate.dblquad(f, kF, np.inf, lambda x: k_0, lambda x: k_inf)
+        res0, err0 = sp.integrate.dblquad(f, k_0, kF,
+                                          lambda x: k_0, lambda x: k_inf)
+        #res1, err1 = sp.integrate.dblquad(f, kF, np.inf,
+        #                                  lambda x: kF, lambda x:
+        #                                  k_inf)
+        # Chunde made a very stupid mistake, in this line, I integrate
+        # in y direction from KF to infinity, it should be from zero
+        # to infinity 
+        res1, err1 = sp.integrate.dblquad(f, kF, np.inf,
+                                          lambda x: k_0, lambda x: k_inf)
         res = res0 + res1
         err = max(err0, err1)
 
     if abs(err) > 1e-6 and abs(err/res) > 1e-6:
         warnings.warn(
-            int_name + " integral did not converge: res, err = %g, %g" % (res, err))
-    return 2*res   # Accounts for integral from -inf to inf for 3D case, shoud be a facotr of 4 instead of 2?
+            f"{int_name} integral did not converge: res, err = {res}, {err}")
+    return 2*res   # Accounts for integral from -inf to inf for 3D
+                   # case, should be a factor of 4 instead of 2? 
+
 
 def get_BCS_v_n_e(delta, mu_eff):
     m = hbar = 1.0
@@ -72,6 +84,7 @@ def get_BCS_v_n_e(delta, mu_eff):
     mu = mu_eff - n*v_0/2
 
     return namedtuple('BCS_Results', ['v_0', 'n', 'mu', 'e'])(v_0, n, mu, e)
+
 
 def BCS(mu_eff, delta=1.0):
     m = hbar = 1.0
@@ -170,11 +183,14 @@ class Homogeneous1D(object):
 
         return namedtuple('BCS_Results', ['v_0', 'ns', 'mus'])(v_0, ns, mus)
 
-"""3D homogenous system with regularization"""
+
 class Homogeneous3D(object):
+    """3D homogenous system with regularization"""
     T = 0.0
     m = hbar = 1
     k_cutoff = np.inf
+    q = 0.0
+    
     def __init__(self, **kw):
         self.__dict__.update(kw)
 
@@ -184,14 +200,14 @@ class Homogeneous3D(object):
         else:
             return 1./(1+np.exp(E/self.T))
 
-    def get_es(self, kz,kp, mus_eff,q=0):
-        return (((kz+q)**2+kp**2)/2.0/m - mus_eff[0],
-                ((kz+q)**2+kp**2)/2.0/m - mus_eff[1])
+    def get_es(self, kz, kp, mus_eff, q):
+        return (((kz+q)**2 + kp**2)/2.0/m - mus_eff[0],
+                ((kz+q)**2 + kp**2)/2.0/m - mus_eff[1])
 
     Results = namedtuple('Results', ['e_p', 'E', 'w_p', 'w_m'])
 
-    def get_res(self, kz,kp, mus_eff, delta,q=0):
-        e_a, e_b = self.get_es(kz,kp, mus_eff=mus_eff,q=q)
+    def get_res(self, kz, kp, mus_eff, delta, q):
+        e_a, e_b = self.get_es(kz, kp, mus_eff=mus_eff,q=q)
         e_p, e_m = (e_a + e_b)/2, (e_a - e_b)/2
         E = np.sqrt(e_p**2 + abs(delta)**2)
         w_p, w_m = e_m + E, e_m - E
@@ -199,90 +215,118 @@ class Homogeneous3D(object):
         rets = self.Results(*[args[_n] for _n in self.Results._fields])
         return rets
 
-
     def get_inverse_scattering_length(self, delta, mus_eff, k_c):
         kF = np.sqrt(2*max(0, max(mus_eff)))
+        
         def gap_integrand(kz_, kp_): 
             # this integration will diverge?
-            res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta)
+            res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta,
+                               q=self.q)
             return kp_* (1 - self.f(res.w_p) - self.f(-res.w_m))/res.E
 
         self._gap_integrand = gap_integrand
 
         if k_c < kF:
-            res, err = sp.integrate.dblquad(gap_integrand, 0, k_c, lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
+            res, err = sp.integrate.dblquad(
+                gap_integrand, 0, k_c,
+                lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
         else:
-            res0, err0 = sp.integrate.dblquad(gap_integrand, 0, kF, lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
-            res1, err1 = sp.integrate.dblquad(gap_integrand, kF, k_c, lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
+            res0, err0 = sp.integrate.dblquad(
+                gap_integrand, 0, kF,
+                lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
+            res1, err1 = sp.integrate.dblquad(
+                gap_integrand, kF, k_c,
+                lambda x: 0, lambda x: np.sqrt(k_c**2-x**2))
             res, err = res0 + res1, err0 + err1
         
         if abs(err) > 1e-6 and abs(err/res) > 1e-6:
-                warnings.warn("scalttering integral did not converge: res, err = %g, %g" % (res, err))
+                warnings.warn("scattering integral did not converge:" +
+                              f"res, err = {res}, {err})")
 
         # Factor of 1/4/pi**2 but we include the factor of 2 from -k_z to k_z
-        res /= 2*np.pi**2 # the result should be the 1/g, where g=-v_0
+        res /= 2*np.pi**2   # the result should be the 1/g, where g=-v_0
 
         # the shift of mu to impprove  convergence, due to 
-        k0 = (np.mean(mus_eff) * 2 * self.m )**0.5 / self.hbar
-        shift_correction = - k_c/2/np.pi**2*k0/2/k_c*np.log((k_c+k0)/(k_c-k0)) *4*np.pi
+        k0 = (np.mean(mus_eff) * 2 * self.m)**0.5 / self.hbar
+
+        # Lambda_c = k_c/2/np.pi**2 (1 - k0/2/k_c * np.log((k_c+k0) / (k_c-k0)))
+        # res = g_inv / 2.0
+        # C = Lambda_c - g_inv
+        # a_inv = 4*np.pi * C
+        shift_correction = (
+            -k_c/2/np.pi**2 * k0/2/k_c
+            * np.log((k_c+k0) / (k_c-k0))
+            *4*np.pi)
         return (-np.pi * 2.0 * res + 2 * k_c / np.pi) + shift_correction
 
-    def get_BCS_v_n_e_in_cylindrical(self, delta, mus_eff, k_c=10000.0, q = 0, unitary = False):
+    def get_BCS_v_n_e_in_cylindrical(self, delta, mus_eff,
+                                     k_c=10000.0,
+                                     unitary=False):
         kF = np.sqrt(2*max(0, max(mus_eff)))
 
-        if not unitary:
-            ainv_s = self.get_inverse_scattering_length(delta=delta, mus_eff=mus_eff, k_c=k_c)
-            v_0 = np.pi * 4.0 / (ainv_s  - 2.0 * k_c/np.pi)
-        else:
-            v_0 = 2 * np.pi **2 / k_c
+        ainv_s = self.get_inverse_scattering_length(
+            delta=delta, mus_eff=mus_eff, k_c=k_c)
 
         def np_integrand(kz_, kp_):
-            res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta)
-            n_p = 1 + res.e_p/res.E*(self.f(res.w_p) + self.f(-res.w_m) - 1)#-->Dr. Forbes's equation
+            res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta,
+                               q=self.q)
+            n_p = 1 + res.e_p/res.E*(
+                self.f(res.w_p) + self.f(-res.w_m) - 1)  # --> Dr. Forbes's equation
             return n_p * kp_
 
         
         def nm_integrand(kz_, kp_):
-            res = self.get_res(kz=kz_,kp=kp_, mus_eff=mus_eff, delta=delta,q=q)
+            res = self.get_res(kz=kz_, kp=kp_, mus_eff=mus_eff,
+                               delta=delta, q=self.q)
             n_m = self.f(res.w_p) - self.f(-res.w_m) #--> Dr. Forbes's equation
             return n_m * kp_
 
-        n_m = dquad(f=nm_integrand, kF=kF, k_inf=k_c, int_name="Density Difference")/4/np.pi**2
-        n_p = dquad(f=np_integrand, kF=kF, k_inf=k_c, int_name="Total Density")/4/np.pi**2
+        n_m = dquad(f=nm_integrand, kF=kF, #k_inf=k_c,
+                    int_name="Density Difference")/4/np.pi**2
+        n_p = dquad(f=np_integrand, kF=kF, #k_inf=k_c,
+                    int_name="Total Density")/4/np.pi**2
         n_a = (n_p + n_m)/2.0
         n_b = (n_p - n_m)/2.0
         ns = np.array([n_a, n_b])
-        mus = mus_eff - np.array([n_b, n_a])*v_0
-        return v_0, ns, mus
+        mus = mus_eff
+        return ainv_s, ns, mus
 
-    def get_BCS_v_n_e_in_spherical(self, delta, mus_eff, k_c=10000.0, unitary = False):
+    def get_BCS_v_n_e_in_spherical(self, delta, mus_eff, k_c=10000.0,
+                                   unitary=False):
         """Return `(v_0, n, mu, e)` for the 3D BCS solution at T=0 or T > 0."""
+        assert self.q == 0
         kF = np.sqrt(2*max(0, max(mus_eff)))
 
         if not unitary:
-            ainv_s = self.get_inverse_scattering_length(delta=delta, mus_eff=mus_eff, k_c=k_c)
+            ainv_s = self.get_inverse_scattering_length(
+                delta=delta, mus_eff=mus_eff, k_c=k_c)
             v_0 = np.pi * 4.0 / (ainv_s  - 2.0 * k_c/np.pi)
         else:
             v_0 = 2 * np.pi **2 / k_c
+            
         def np3(kr):
              res = self.get_res(kz=kr,kp=0, mus_eff=mus_eff, delta=delta)
              n_p = 1 + res.e_p/res.E*(self.f(res.w_p) + self.f(-res.w_m) - 1)
              return n_p
+         
         def np_integrand(kr):
             return np3(kr) * kr**2
 
         self._np3 = np3
+        
         def nm_integrand(kr):
             res = self.get_res(kz=kr,kp=0, mus_eff=mus_eff, delta=delta)
             n_m = self.f(res.w_p) - self.f(-res.w_m)
             return n_m * kr**2
 
-        n_m = sp.integrate.quad(nm_integrand, 0, k_c)[0]/2/np.pi**2#quad(f=nm_integrand, kF=kF)/2/np.pi**2#check the factor, should change
-        n_p = sp.integrate.quad(np_integrand, 0, k_c)[0]/2/np.pi**2#quad(f=np_integrand, kF=kF)/2/np.pi**2#check the factor, should change
+        n_m = sp.integrate.quad(nm_integrand, 0, k_c)[0]/2/np.pi**2
+        #quad(f=nm_integrand, kF=kF)/2/np.pi**2#check the factor, should change
+        n_p = sp.integrate.quad(np_integrand, 0, k_c)[0]/2/np.pi**2
+        #quad(f=np_integrand, kF=kF)/2/np.pi**2#check the factor, should change
         n_a = (n_p + n_m)/2.0
         n_b = (n_p - n_m)/2.0
         ns = np.array([n_a, n_b])
-        mus = mus_eff - np.array([n_b, n_a])*v_0
+        mus = mus_eff
 
         return v_0, ns, mus
 
