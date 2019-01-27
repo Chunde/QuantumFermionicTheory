d7cdc13d004995c7b78d5161a0a6476178aed3cc
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-01-26 21:35:26 -0800

    BLD,ENH,TST: Added tf_correction code, tests, and config files.
    .hgignore:
    - Added.  Ignore some cache directories, coverage output etc.
    
    quantum_turbulence/ASLDA.py:
    - Move this to the _trash directory... until this code has a change of
      passing tests, it should not be included in the main directory.
    
    pytest.ini:
    - This provides configuration options for py.test.  In particular, it
      triggers coverage testing.
    
    setup.cfg:
    - Some other configuration options for linting, etc.
    
    quantum_turbulence/tf_completion.py:
    quantum_turbulence/test_tf_completion.py:
    - Main code for integrals...  Will use numba for performance (see the
      next DOC update.)

---
+++ quantum-turbulence/test_tf_completion.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy.optimize import brentq
+
+import py.test
+
+
+import tf_completion
+
+
+def test_3D():
+    """Test the 3D UFG solution."""
+    np.random.seed(1)
+    m, hbar, kF = 1 + np.random.random(3)
+    eF = (hbar*kF)**2/2/m
+    nF = kF**3/3/np.pi**2
+    mu = 0.59060550703283853378393810185221521748413488992993*eF
+    delta = 0.68640205206984016444108204356564421137062514068346*eF
+    args = dict(mu_a=mu, mu_b=mu, delta=delta, m_a=m, m_b=m, hbar=hbar, T=0.0)
+    n_p = tf_completion.integrate(tf_completion.n_p_integrand, d=3, **args)
+    assert np.allclose(n_p.n, nF)
+
+
+def test_2D():
+    """Test the 2D UFG solution."""
+    np.random.seed(1)
+    m, hbar, kF = 1 + np.random.random(3)
+    eF = (hbar*kF)**2/2/m
+    nF = kF**2/2/np.pi
+    mu = 0.5*eF
+    delta = np.sqrt(2)*eF
+    args = dict(mu_a=mu, mu_b=mu, delta=delta, m_a=m, m_b=m, hbar=hbar, T=0.0)
+    n_p = tf_completion.integrate(tf_completion.n_p_integrand, d=2, **args)
+    assert np.allclose(n_p.n, nF)
+
+
+def test_1D():
+    """Test a few values from Table I of Quick:1993."""
+    lam_inv = 0.5
+    np.random.seed(1)
+    m, hbar, delta = 0.1 + np.random.random(3)
+    lam = 1./lam_inv
+    import homogeneous
+
+    def _lam(mu_eff):
+        E_N_E_2, _lam = homogeneous.BCS(mu_eff=mu_eff,  delta=delta)
+        return _lam - lam
+
+    mu_eff = brentq(_lam, 0.1, 20)
+
+    args = dict(mu_a=mu_eff, mu_b=mu_eff, delta=delta, m_a=m, m_b=m,
+                hbar=hbar, T=0.0)
+    n_p = tf_completion.integrate(tf_completion.n_p_integrand, d=1, **args)
+    nu = tf_completion.integrate(tf_completion.nu_integrand, d=1, **args)
+    v_0 = delta/nu.n
+    mu = mu_eff - n_p.n*v_0/2
+    lam = m*v_0/n_p.n/hbar**2
+    
+    #v_0, n, mu, e = homogeneous.get_BCS_v_n_e(mu_eff=mu_eff, delta=delta)
+    #E_N_E_2, lam = homogeneous.BCS(mu_eff=mu_eff,  delta=delta)
+    mu_tilde = (hbar**2/m/v_0**2)*mu
+    
+    assert np.allclose(lam, 1./lam_inv)
+    assert np.allclose(mu_tilde, 0.0864, atol=0.0005)    
+    #assert np.allclose(E_N_E_2, -0.3037, atol=0.0005)
---
+++ quantum-turbulence/tf_completion.py
@@ -0,0 +1,133 @@
+"""Numba functions for fast integration of homogeneous matter."""
+
+import math
+
+import numpy as np
+import numba
+
+from scipy.integrate import quad
+import scipy as sp
+
+from uncertainties import ufloat
+
+#@numba.jit(nopython=True)
+def f(E, T):
+    """Fermi distribution function."""
+    if T == 0:
+        return (1 - np.sign(E))/2
+    else:
+        return 1./(1+np.exp(E/T))
+
+
+######################################################################
+# These *_integrand functions do not have the integration measure
+# factors, so they can be used for any dimension (but need an
+# appropriate wrapper).
+    
+#@numba.jit(nopython=True)
+def n_p_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_nu = (f(w_m, T) - f(w_p, T))
+    f_p = 1 - e_p/E*f_nu
+    return f_p
+
+
+#@numba.jit(nopython=True)
+def n_m_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_m = f(w_p, T) - f(-w_m, T)
+    return f_m
+
+
+#@numba.jit(nopython=True)
+def tau_p_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_nu = (f(w_m, T) - f(w_p, T))
+    f_p = 1 - e_p/E*f_nu
+    return k2*f_p
+
+
+#@numba.jit(nopython=True)
+def tau_m_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_m = f(w_p, T) - f(-w_m, T)
+    return k2*f_m
+
+
+#@numba.jit(nopython=True)
+def nu_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_nu = (f(w_m, T) - f(w_p, T))
+    return delta/2/E*f_nu
+
+
+#@numba.jit(nopython=True)
+def kappa_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_nu = (f(w_m, T) - f(w_p, T))
+    f_p = 1 - e_p/E*f_nu
+    m_p_inv = (1/m_a + 1/m_b)/2
+    return e*m_p_inv*f_p + abs(delta)**2/2/E*f_nu
+
+
+#@numba.jit(nopython=True)
+def C_integrand(k2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+    e = hbar**2*k2/2
+    e_a, e_b = e/m_a - mu_a, e/m_b - mu_b
+    e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+    E = np.sqrt(e_p**2 + abs(delta)**2)
+    w_m, w_p = e_m - E, e_m + E
+    f_nu = (f(w_m, T) - f(w_p, T))
+    return f_nu/2*(1/e_p - 1/E)
+
+
+def integrate(f, mu_a, mu_b, delta, m_a, m_b, d=3, hbar=1.0, T=0.0):
+    args = (mu_a, mu_b, delta, m_a, m_b, hbar, T)
+    if d == 1:
+        def integrand(k):
+            return f(k**2, *args) / np.pi
+    elif d == 2:
+        def integrand(k):
+            return f(k**2, *args) * (k/2/np.pi)
+    elif d == 3:
+        def integrand(k):
+            k2 = k**2
+            return f(k2, *args) * (k2/(2*np.pi**2))
+    else:
+        raise ValueError(f"Only d=1, 2, or 3 supported (got d={d})")
+    
+    #integrand = numba.cfunc(numba.float64(numba.float64))(integrand)
+    #integrand =sp.LowLevelCallable(integrand.ctypes)
+
+    mu = (mu_a + mu_b)/2
+    minv = (1/m_a + 1/m_b)/2
+    kF = math.sqrt(2*mu/minv)/hbar
+    points = [kF]
+    
+    return (ufloat(*quad(integrand, 0, max(points), points=points))
+            +ufloat(*quad(integrand, max(points), np.inf)))        
+    
