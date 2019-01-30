WIP: Done with the error analysis(for 2d_vortex class, nu part error  may not right, need to double check). Fix some errors in notebooks, remove minus sign for Delta.

quantum-turbulence/test_ASLDA_Homogeneous.py
@@ -57,7 +57,7 @@ class ASLDA_(vortex_1d_aslda.ASLDA):
 def test_ASLDA_Homogenous():
     L = 0.46
     N = 16
-    N_twist = 64
+    N_twist = 128
     delta = 1.0
     mu_eff = 1.0
     v_0, n, mu, e_0 = homogeneous.get_BCS_v_n_e(delta=delta, mu_eff=mu_eff)
@@ -71,26 +71,26 @@ def test_ASLDA_Homogenous():
     na = np.diag(R)[:N]/b.dx
     nb = (1 - np.diag(R)[N:])/b.dx
     kappa = np.diag(R[:N, N:])/b.dx
-    print((n, na[0].real + nb[0].real), (delta, v_0*kappa[0].real))
+    print((n, na[0].real + nb[0].real), (delta, -v_0*kappa[0].real))
     assert np.allclose(n_, na.real + nb.real,atol=0.001)
-    #assert np.allclose(delta, v_0*kappa[0].real,atol=0.001)
+    assert np.allclose(delta, -v_0*kappa[0].real,atol=0.01)
     print("Test 1d lattice with homogeneous system")
     ns,taus,kappa = b.get_ns_taus_kappa_average_1d(mus=(mu_eff  * np.ones((N),), mu_eff  * np.ones((N),)), delta=delta * np.ones((N),), N_twist=N_twist)
     na,nb = ns
-    print((n, na[0].real + nb[0].real), (delta, v_0*kappa[0].real))
+    print((n, na[0].real + nb[0].real), (delta, -v_0*kappa[0].real))
     assert np.allclose(n_, na.real + nb.real,atol=0.001)
-    #assert np.allclose(delta, v_0*kappa[0].real,atol=0.001)
+    assert np.allclose(delta, -v_0*kappa[0].real,atol=0.01)
     print("Test 1d lattice plus 1d integral over y with homogeneous system")
     ns,taus,kappa = b.get_ns_taus_kappa_average_2d(mus=(mu_eff  * np.ones((N),), mu_eff  * np.ones((N),)), delta=delta * np.ones((N),), N_twist=N_twist)
     na,nb = ns
-    print((n, na[0].real + nb[0].real), (delta, v_0*kappa[0].real))
+    print((n, na[0].real + nb[0].real), (delta, -v_0*kappa[0].real))
     assert np.allclose(n_, na.real + nb.real,atol=0.001)
-    #assert np.allclose(delta, v_0*kappa[0].real,atol=0.001)
+    assert np.allclose(delta, -v_0*kappa[0].real,atol=0.01)
     print("Test 1d lattice plus 2d integrals over y and  z with homogeneous system")
     ns,taus,kappa = b.get_ns_taus_kappa_average_3d(mus=(mu_eff  * np.ones((N),), mu_eff  * np.ones((N),)), delta=delta * np.ones((N),), N_twist=N_twist)
     na,nb = ns
-    print((n, na[0].real + nb[0].real), (delta, v_0*kappa[0].real))
-    assert np.allclose(n_, na.real + nb.real,atol=0.001)
+    print((n, na[0].real + nb[0].real), (delta, -v_0*kappa[0].real))
+    assert np.allclose(n_, na.real + nb.real,atol=0.01)
     #assert np.allclose(delta, v_0*kappa[0].real,atol=0.001)
 
 

quantum-turbulence/vortex_1d_aslda.py
@@ -337,8 +337,8 @@ class ASLDA(object):
         #K_a,K_b = self.get_modified_Ks(alpha_a=alpha_a,alpha_b=alpha_b,twist = twist)
         Mu_a, Mu_b = np.diag((mu_a - V_a).ravel()), np.diag((mu_b - V_b).ravel())
         assert (Mu_a.shape[0] == self.x.shape[0])
-        H = np.bmat([[K_a - Mu_a, -Delta], # may need remove the minus sign for Delta, need to check
-                     [-Delta.conj(), -(K_b - Mu_b)]]) 
+        H = np.bmat([[K_a - Mu_a, Delta], # may need remove the minus sign for Delta, need to check
+                     [Delta.conj(), -(K_b - Mu_b)]]) 
         assert np.allclose(H.real,H.conj().T.real)
         return np.asarray(H)
 
@@ -390,8 +390,8 @@ class ASLDA(object):
                 def f(ky=0):
                     k_per = self.hbar**2/2/self.m  *ky**2
                     K_a,K_b = self.get_modified_Ks(alpha_a=alpha_a,alpha_b=alpha_b,twist = twist)
-                    H = np.bmat([[K_a - Mu_a, -Delta], # may need remove the minus sign for Delta, need to check
-                         [-Delta.conj(), -(K_b - Mu_b)]]) 
+                    H = np.bmat([[K_a - Mu_a, Delta], # may need remove the minus sign for Delta, need to check
+                         [Delta.conj(), -(K_b - Mu_b)]]) 
                     assert np.allclose(H.real,H.conj().T.real)
                     _ns,_taus,_kappa = self.get_ns_taus_kappa(np.asarray(H))
                     return np.concatenate((_ns[0].ravel(),_ns[1].ravel(),_taus[0].ravel(),_taus[1].ravel(),_kappa.ravel()))
@@ -424,8 +424,8 @@ class ASLDA(object):
             def f(ky=0):
                 k_per = self.hbar**2/2/self.m  *ky**2
                 K_a,K_b = self.get_modified_Ks(alpha_a=alpha_a,alpha_b=alpha_b,twist = twist)
-                H = np.bmat([[K_a - Mu_a, -Delta], # may need remove the minus sign for Delta, need to check
-                     [-Delta.conj(), -(K_b - Mu_b)]]) 
+                H = np.bmat([[K_a - Mu_a, Delta], # may need remove the minus sign for Delta, need to check
+                     [Delta.conj(), -(K_b - Mu_b)]]) 
                 assert np.allclose(H.real,H.conj().T.real)
                 _ns,_taus,_kappa = self.get_ns_taus_kappa(np.asarray(H))
                 return np.concatenate((_ns[0].ravel(),_ns[1].ravel(),_taus[0].ravel(),_taus[1].ravel(),_kappa.ravel()))

quantum-turbulence/vortex_2d.py
@@ -119,8 +119,26 @@ class BCS_1D(object):
                      [Delta.conj(), -(K_b - Mu_b)]])
         return np.asarray(H)
 
-    def get_Rs(self, mus, delta):
+    def get_Rs(self, mus, delta,N_twist = 1):
         """Return the density matrix R."""
+
+        Rps = []
+        Rms = []
+        twists = np.arange(0, N_twist)*2*np.pi/N_twist
+
+        for twist in twists:
+            H = self.get_H(mus=mus, delta=delta, twist=twist)
+            d, UV = np.linalg.eigh(H)
+            Rp = UV.dot(self.f(d)[:, None]*UV.conj().T)
+            Rm = UV.dot(self.f(-d)[:, None]*UV.conj().T)
+            Rps.append(Rp)
+            Rms.append(Rm)
+        Rp = sum(Rps)/len(Rps)
+        Rm = sum(Rms)/len(Rms)
+        return Rp/self.dx, Rm/self.dx
+
+
+
         H = self.get_H(mus=mus, delta=delta)
         d, UV = np.linalg.eigh(H)
         dV = self.dx
@@ -130,8 +148,8 @@ class BCS_1D(object):
         Rm = UV.dot(self.f(-d)[:, None]*UV.conj().T) / dV
         return Rp, Rm
 
-    def get_densities(self, mus, delta):
-        Rp, Rm = self.get_Rs(mus=mus, delta=delta)
+    def get_densities(self, mus, delta,N_twist=1):
+        Rp, Rm = self.get_Rs(mus=mus, delta=delta,N_twist=N_twist)
         _N = Rp.shape[0] // 2
         r_a = Rp[:_N, :_N]
         r_b = Rm[_N:, _N:].conj()