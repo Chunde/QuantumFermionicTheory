4b49e71e4f3239c7893ab41a600a2af81c19e696
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-02-01 09:07:38 -0800

    DOC: Cleanup documentation initline.  Move nbinit to nbimports.
    Use the following at the start of Docs/*.ipynb notebooks.
    
    import mmf_setup;mmf_setup.nbinit()
    %pylab inline --no-import-all
    from nbimports import *            # Convenience functions like clear_output

---
+++ mmf-hfb/Docs/nbimports.py
@@ -0,0 +1,39 @@
+"""Notebook initialization if mmf_setup.nbinit() fails.
+
+Include something like the following in your first cell:
+
+import mmf_setup;mmf_setup.nbinit()
+%pylab inline --no-import-all
+from nbinit import *                # Conveniences like clear_output
+
+Note: you must start your Jupyter notebook in this directory for this
+to work.
+
+"""
+__all__ = ['reload', 'display', 'clear_output', 'NoInterrupt',
+           'mmf_setup', 'imcontourf']
+
+try:
+    reload  # Python 2.7
+except NameError:
+    try:
+        from importlib import reload  # Python 3.4+
+    except ImportError:
+        from imp import reload  # Python 3.0 - 3.3
+
+import os.path
+import sys
+import mmf_setup
+
+try:
+    mmf_setup.nbinit()
+    sys.path.insert(os.path.join(mmf_setup.HGROOT, 'mmf-hfb'))
+except:
+    mmf_setup.nbinit(hgroot=False)
+    DIR = os.path.abspath(os.path.dirname(__file__))
+    sys.path.insert(0, os.path.join(DIR, '..'))
+
+from IPython.display import display, clear_output
+
+from mmfutils.contexts import NoInterrupt
+from mmfutils.plot import imcontourf
