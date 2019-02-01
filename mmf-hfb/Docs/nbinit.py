5d947e1961bc1e8dacbb98c19b4778717d87c303
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-01-31 20:45:30 -0800

    BLD: Added new nbinit for notebooks that works on windows (start j in Docs)

--- mmf-hfb/Docs/nbinit.py
+++ mmf-hfb/Docs/nbinit.py
@@ -2,21 +2,19 @@
 
 Include the following in your first cell:
 
-    import mmf_setup
-    try: mmf_setup.nbinit()
-    except: import nbinit
+    from nbinit import *
 
 Note: you must start your Jupyter notebook in this directory for this
 to work.
 
 """
+import os.path
+import sys
 import mmf_setup
-
 try:
     mmf_setup.nbinit()
+    sys.path.insert(os.path.join(mmf_setup.HGROOT, 'mmf-hfb'))
 except:
-    import sys
-    sys.path.insert(0, '../')
     mmf_setup.nbinit(hgroot=False)
-
-
+    DIR = os.path.abspath(os.path.dirname(__file__))
+    sys.path.insert(0, os.path.join(DIR, '..'))
