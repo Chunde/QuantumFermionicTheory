6ba7cfcec4f25ff68ff196b13e2b86002dffbade
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-02-16 00:23:03 -0800

    ENH,TST: Added integrate.dquad and tested it.

--- mmf-hfb/mmf_hfb/integrate.py
+++ mmf-hfb/mmf_hfb/integrate.py
@@ -1,10 +1,9 @@
 """Numerical integration routines."""
-import math
+from functools import partial
 import numpy as np
 import scipy.integrate
 import scipy as sp
 from uncertainties import ufloat
-import warnings
 
 
 def quad(func, a, b, points=None, **kw):
@@ -22,3 +21,36 @@ def quad(func, a, b, points=None, **kw):
         return sign * sum(ufloat(*_r) for _r in res)
     else:
         return ufloat(*sp.integrate.quad(func=func, a=a, b=b, points=points))
+
+
+def dquad(func, x0, x1, y0_x, y1_x, points_x=None, points_y_x=None, **kw):
+    """Compute a double integral.
+
+    Note: The order of arguments is not the same as dblquad.  They are
+    func(x, y) here.
+    
+    Arguments
+    ---------
+    func : callable
+       A Python function or method of at least two variables: f(x, y)
+    x0, x1 : float
+       The limits of integration in x: x0 < x1
+    x0_x, y1_x : callable
+       The lower (upper) boundary curve in y which is a function taking a
+       single floating point argument (x) and returning a floating point
+       result or a float indicating a constant boundary curve.
+    points_x : []
+       List of special points in x (outer integral)
+    points_y_x : callable or list
+       points_y_x(x) should return a list of special points in y for
+       the inner integral.
+    """
+    def inner_integrand(x):
+        points = None
+        if points_y_x is not None:
+            points = points_y_x(x)
+        
+        return quad(partial(func, x), a=y0_x(x), b=y1_x(x),
+                    points=points, **kw).n
+
+    return quad(inner_integrand, x0, x1, points=points_x, **kw)
--- mmf-hfb/tests/test_integrate.py
+++ mmf-hfb/tests/test_integrate.py
@@ -40,3 +40,64 @@ class TestQuad(object):
         res = integrate.quad(f, a, b, points=points)
         assert np.allclose(res.n, exact)
         assert abs(res.n - exact) < 10*res.s
+
+
+def cases2():
+    def f(x, y, x0=0, y0=0, np=np):
+        return (1 + (x-x0) - 2*(y-y0))*np.exp(-(x-x0)**2 - (y-y0)**2)
+
+    x, y = sympy.var(['x', 'y'], real=True)
+    f_xy = f(x, y, x0=0, y0=0, np=sympy)
+    _exact = f_xy.integrate((x, -np.inf, np.inf),
+                            (y, -np.inf, np.inf))
+    exact = float(_exact.evalf(17))
+
+    _exact = f_xy.integrate((x, -1, 1),
+                            (y, -1, 1))
+    exact1 = float(_exact.evalf(17))
+
+    yield (f, -np.inf, np.inf, -np.inf, np.inf, None, None, exact)
+    yield (f, np.inf, -np.inf, np.inf, -np.inf, None, None, exact)
+    yield (f, -1, 1, -1, 1, None, None, exact1)
+    
+    for x0 in [0, 1e5]:
+        for y0 in [0, 1e5]:
+            f_ = partial(f, x0=x0, y0=y0)
+            yield (f_, -np.inf, np.inf, -np.inf, np.inf, [x0], [y0], exact)
+            yield (f_, np.inf, -np.inf, np.inf, -np.inf, [x0], [y0], exact)
+            yield (f_, x0-1, x0+1, y0-1, y0+1, [x0], [y0], exact1)
+
+
+@pytest.fixture(params=list(cases2()))
+def f2_a_b_points_exact(request):
+    yield request.param
+
+    
+class TestdQuad(object):
+    def test(self, f2_a_b_points_exact):
+        f, x0, x1, y0, y1, points_x, points_y, exact = f2_a_b_points_exact
+
+        def y0_x(x):
+            return y0
+        
+        def y1_x(x):
+            return y1
+
+        if points_y is None:
+            points_y_x = None
+        else:
+            def points_y_x(x):
+                return points_y
+    
+        res = integrate.dquad(f, x0, x1, y0_x, y1_x, points_x, points_y_x)
+        assert np.allclose(res.n, exact)
+        assert abs(res.n - exact) < 20*res.s
+
+        # Check that switching limits switches sign
+        res = integrate.dquad(f, x1, x0, y0_x, y1_x, points_x, points_y_x)
+        assert np.allclose(res.n, -exact)
+        assert abs(res.n + exact) < 20*res.s
+
+        res = integrate.dquad(f, x0, x1, y1_x, y0_x, points_x, points_y_x)
+        assert np.allclose(res.n, -exact)
+        assert abs(res.n + exact) < 20*res.s
