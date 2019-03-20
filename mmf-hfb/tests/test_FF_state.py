ae5a438b30f1c0e1ff13171977b548a13a0a0b36
Chunde <xmuhcd@msn.com>
2019-03-20 00:30:52 -0700

    DOC: Update Notebook on thermodynamic relations

--- mmf-hfb/tests/test_FF_state.py
+++ mmf-hfb/tests/test_FF_state.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from mmf_hfb import tf_completion as tf
 from mmf_hfb import bcs, homogeneous
+from scipy.optimize import brentq
 
 from mmf_hfb.FuldeFerrelState import FFState as FF
 tf.MAX_DIVISION = 200
@@ -78,45 +79,74 @@ def get_dE_dn(mu, dmu, dim, q=0, dq=0):
     e2, n2 = get_e_n(mu=mu - dx, dmu=dmu, dim=dim, q=q, dq=dq)
     return (e1-e2)/(sum(n1)-sum(n2))
 
-def Thermodynamic(mu, dmu, delta0=1, dim=1, k_c=100, q=0, dq=0, T=0.0, dx=1e-6):
-    print(f"mu={mu}\tdmu={dmu}\tkc={k_c}\tq={q}\tdq={dq}\tdim={dim}")    
+def Thermodynamic(mu, dmu,delta0=1, dim=1, k_c=100, q=0, dq=0, T=0.0, dx=1e-3):
+    #print(f"mu={mu}\tdmu={dmu}\tkc={k_c}\tq={q}\tdq={dq}\tdim={dim}")    
     ff = FF(mu=mu, delta=delta0, dim=dim, k_c=k_c, T=T, fix_g=True)
-
+   
     def get_P(mu, dmu):
         delta = ff.solve(mu=mu, dmu=dmu, q=q, dq=dq, a=0.8*delta0, b=1.2*delta0)
         return ff.get_pressure(mu=mu, dmu=dmu, delta=delta, q=q, dq=dq)
 
     def get_E_n(mu, dmu):
         E = ff.get_energy_density(mu=mu, dmu=dmu, q=q, dq=dq)
-        n = sum(ff.get_densities(mu=mu, dmu=dmu, q=q, dq=dq))
-        return E, n
+        na, nb = ff.get_densities(mu=mu, dmu=dmu, q=q, dq=dq)
+        return E, na, nb
 
     def get_ns(mu, dmu):
         return ff.get_densities(mu=mu, dmu=dmu, q=q, dq=dq)
-    energy_density, (na, nb) = get_e_n(mu=mu, dmu=dmu, dim=dim)        
-    print(f"{dim}D: E={energy_density}\tn_a={na}\tn_b={nb}\tn_p={na+nb}")    
-    E1, n1 = get_E_n(mu=mu+dx, dmu=dmu)
-    E0, n0 = get_E_n(mu=mu-dx, dmu=dmu)
-    print(f"E1={E1.n}\tE0={E0.n}\tn1={n1.n}\tn0={n0.n}")
-    n_p = (get_P(mu+dx, dmu) - get_P(mu-dx, dmu))/2/dx
+    
+    
+    def f_ns_dmu(dx, n):
+        na, nb = ff.get_densities(mu=mu, dmu=dmu, q=q, dq=dq)
+        dn = (na - nb).n
+        def f(dmu_):
+            na_, nb_ = ff.get_densities(mu=mu+dx, dmu=dmu_, q=q, dq=dq)
+            if n == 0: # fix n_-
+                dn_ = (na_ - nb_).n
+                return dn - dn_
+            elif n == 1: # fix n_b
+                return (nb - nb_).n
+            elif n == 2: # fix n_a
+                return (na - na_).n
+        return brentq(f, -3*dmu , 3*dmu)
+    
+
+    dmu1 = f_ns_dmu(dx, 0)
+    dmu2 = f_ns_dmu(-dx, 0)
+    E1, na1, nb1 = get_E_n(mu=mu+dx, dmu=dmu1)
+    E0, na0, nb0 = get_E_n(mu=mu-dx, dmu=dmu2)
+    n1, n0 = (na1 + nb1).n, (na0 + nb0).n
+    mu_ = ((E1-E0)/(n1-n0)).n
+    print(f"Expected mu={mu}\tNumerical mu={mu_}")
+    assert np.allclose(mu,mu_)
+    dmu1 = f_ns_dmu(dx, 1)
+    dmu2 = f_ns_dmu(-dx, 1)
+    E1, na1, nb1 = get_E_n(mu=mu+dx, dmu=dmu1)
+    E0, na0, nb0 = get_E_n(mu=mu-dx, dmu=dmu2)
+    print(f"na1={na1}\tnb1={nb1}\tna0={na0}\tnb0{nb0}")
+
+    n1, n0 = (na1 + nb1).n, (na0 + nb0).n
+    mu_ = ((E1-E0)/(n1-n0)).n
+    print(f"Expected mu_a={mu + dmu}\tNumerical mu_a={mu_}")
+    assert np.allclose(mu + dmu,mu_)
+    dmu1 = f_ns_dmu(dx, 2)
+    dmu2 = f_ns_dmu(-dx, 2)
+    E1, na1, nb1 = get_E_n(mu=mu+dx, dmu=dmu1)
+    E0, na0, nb0 = get_E_n(mu=mu-dx, dmu=dmu2)
+    n1, n0 = (na1 + nb1).n, (na0 + nb0).n
+    mu_ = ((E1-E0)/(n1-n0)).n
+    print(f"Expected mu_b={mu - dmu}\tNumerical mu_b={mu_}")
+    assert np.allclose(mu - dmu,mu_)
+    
     n_a, n_b = get_ns(mu, dmu)
     n_a_ = (get_P(mu+dx/2, dmu+dx/2) - get_P(mu-dx/2, dmu - dx/2))/2/dx
     n_b_ = (get_P(mu+dx/2, dmu-dx/2) - get_P(mu-dx/2, dmu + dx/2))/2/dx
-    print(f"n_a={n_a.n}\tNumerical  n_a={n_a_.n}")
-    print(f"n_b={n_b.n}\tNumerical  n_b={n_b_.n}")
-    print(f"n_p={n_a.n+n_b.n}\tNumerical  n_p={n_p.n}")
-    print(f"mu={mu}\tNumerical mu={((E1-E0)/(n1-n0)).n}")
-    if dim == 1:
-        rtol = 1e-5
-    else:
-        rtol = 1e-2 # For 2d and 3d, the accurary is not high, I need to figure out the reason!
-    assert np.allclose(n_a.n, n_a_.n, rtol=rtol)
-    assert np.allclose(n_b.n, n_b_.n, rtol=rtol)
-    assert np.allclose(n_p.n, (n_a+n_b).n, rtol=rtol)
-    if not np.allclose(((E1-E0)/(n1-n0)).n, get_dE_dn(mu=mu,dmu=dmu,dim=dim, q=q, dq=dq)):# if is superfluid(Delta !=0)
-        assert np.allclose(mu,((E1-E0)/(n1-n0)).n, rtol=rtol)
+    print(f"Expected n_a={n_a.n}\tNumerical n_a={n_a_.n}")
+    print(f"Expected n_b={n_b.n}\tNumerical n_b={n_b_.n}")
+    assert np.allclose(n_a.n, n_a_.n)
+    assert np.allclose(n_b.n, n_b_.n)
 
 
 if __name__ == "__main__":
     # test_Thermodynamic(mu=5, dmu=0.0, k_c=200, q=0.0, dq=0.0, dim=1)
-    Thermodynamic(mu=5, dmu=2.5, k_c=200, q=0, dq=.0, dim=1)
+    Thermodynamic(mu=5, dmu=.5, k_c=200, q=0, dq=.0, dim=1)
