TST, WIP: Add current intergration in tf_completion. Fix test errors.  Need to check the current calculation.

mmf-hfb/Docs/ConstructFFStates.py
@@ -21,6 +21,7 @@ import mmf_setup;mmf_setup.nbinit()
 from nbimports import * 
 from mmf_hfb import tf_completion as tf
 from mmf_hfb.FuldeFerrelState import FFState
+from mmf_hfb.FFStateFinder import FFStateFinder
 from scipy.optimize import brentq
 from mmfutils.plot import imcontourf
 clear_output()
@@ -44,8 +45,8 @@ import json
 import glob
 from json import dumps
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-pattern = join(currentdir,"..","mmf_hfb","FFState*.json")
-files = files=glob.glob(pattern)
+pattern = join(currentdir,"..","mmf_hfb","FFState_[_0-9]*.json")
+files = glob.glob(pattern)
 plt.figure(figsize=(20,7))
 for file in files:
     if os.path.exists(file):
@@ -76,6 +77,77 @@ plt.xlabel(f"$\Delta$")
 plt.ylabel(f"$\delta q$")
 plt.title(f"$\mu=${mu},$\Delta=${delta}. Upper Branch")
 plt.legend()
+
+# ## Compute Current and Pressure
+# $$
+# J=\int dk\left[(k+\delta q) f_a + (k - \delta q) f_b\right]
+# $$
+
+import os
+import inspect
+from os.path import join
+import json
+import glob
+import warnings
+warnings.filterwarnings("ignore")
+from json import dumps
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+pattern = join(currentdir,"..","mmf_hfb","FFState_J_P*.json")
+files=glob.glob(pattern)
+plt.figure(figsize=(20,20))
+for file in files:
+    file = files[2]
+    if os.path.exists(file):
+        with open(file,'r') as rf:
+            ret = json.load(rf)
+            dim, mu, dmu, delta=ret['dim'], ret['mu'], ret['dmu'], ret['delta']
+            ff = FFState(mu=mu, dmu=dmu, delta=delta, dim=dim)
+            p0 = ff.get_pressure(mu=mu, dmu=dmu, mu_eff=mu, dmu_eff=dmu,delta=0).n
+            print(p0)
+            data1, data2 = ret['data']
+            dqs1, dqs2, ds1, ds2, j1, j2, P1, P2 = [],[],[],[],[],[],[],[]
+            for data in data1:
+                d, q, p, j = data['d'],data['q'],data['p'],data['j']
+                ds1.append(d)
+                dqs1.append(q)
+                j1.append(j)
+                P1.append(p)
+            for data in data2:
+                d, q, p, j = data['d'],data['q'],data['p'],data['j']
+                ds2.append(d)
+                dqs2.append(q)
+                j2.append(j)
+                P2.append(p)
+                
+            plt.subplot(321)
+            plt.plot(ds1, dqs1, label=f"$d\mu=${dmu}")
+            plt.subplot(322)
+            plt.plot(ds2, dqs2, label=f"$d\mu=${dmu}")
+            plt.subplot(323)
+            plt.plot(ds1, P1, label=f"$d\mu=${dmu}")
+            plt.subplot(324)
+            #plt.axhline(p0)
+            plt.plot(ds2, P2, label=f"$d\mu=${dmu}")
+            plt.subplot(325)
+            plt.plot(ds1, j1, label=f"$d\mu=${dmu}")
+            plt.subplot(326)
+            plt.plot(ds2, j2, label=f"$d\mu=${dmu}")
+            
+    for i in range(1,7):
+        plt.subplot(3,2,i)
+        plt.legend()
+        if i == 1:
+            plt.title(f"Lower Branch: $\Delta$={delta},$\mu=${mu},$d\mu=${dmu}")
+            plt.ylabel("$\delta q$")
+        if i == 2:
+            plt.title(f"Upper Branch Branch: $\Delta$={delta},$\mu=${mu},$d\mu=${dmu}")
+            plt.ylabel("$\delta q$")
+        if i == 3 or i == 4:
+            plt.ylabel("$Pressure$")
+        if i == 5 or i == 6:
+            plt.ylabel("$Current$")
+        plt.xlabel("$\Delta$")
+    break
 
 # ## Mathematical Relations:
 # \begin{align}

mmf-hfb/mmf_hfb/FFStateFinder.py
@@ -1,6 +1,7 @@
 from mmf_hfb import tf_completion as tf
 from mmf_hfb.FuldeFerrelState import FFState
 from scipy.optimize import brentq
+from multiprocessing import Pool
 #import warnings
 #warnings.filterwarnings("ignore")
 import sys
@@ -11,16 +12,17 @@ import json
 from json import dumps
 import numpy as np
 import time
+import glob
 
 class FFStateFinder():
-    def __init__(self, dim=1, delta=0.1, mu=10.0, dmu=0.11):
+    def __init__(self, dim=1, delta=0.1, mu=10.0, dmu=0.11, prefix="FFState_"):
         self.dim = dim
         self.delta = delta
         self.mu = mu
         self.dmu = dmu
-        self.fileName = "FFState_" + time.strftime("%Y_%m_%d_%H_%M_%S.json")
+        self.fileName = prefix + time.strftime("%Y_%m_%d_%H_%M_%S.json")
         self.ff = FFState(mu=mu, dmu=dmu, delta=delta, dim=1,
-                         k_c=np.inf,fix_g=True, bStateSentinel=True)
+                         k_c=np.inf, fix_g=True, bStateSentinel=True)
 
     def _gc(self, delta, dq, update_mus=True):
         mus_eff = (None, None)
@@ -28,14 +30,34 @@ class FFStateFinder():
             mus_eff = self.ff._get_effetive_mus(mu=self.mu, dmu=self.dmu, delta=delta, dq=dq, update_g=False)
         return self.ff.get_g(mu=mus_eff[0], dmu=mus_eff[1], delta=delta, dq=dq) - self.ff._g
 
-    def get_press(self, dq):
-        mu_eff, dmu_eff = self.ff._get_effetive_mus(mu=self.mu, dmu=self.dmu, delta=self.delta, dq=dq, update_g=False)
-        n_a, n_b = self.ff.get_densities(mu=mu_eff, dmu=dmu_eff, delta=self.delta, dq=dq)
-        energy_density = self.ff.get_energy_density(mu=mu_eff, dmu=dmu_eff, delta=self.delta, dq=dq, n_a=n_a, n_b=n_b)
+    def get_mus_eff(self, delta, dq, mus_eff=None):
+        """return effective mus"""
+        return self.ff._get_effetive_mus(mu=self.mu, dmu=self.dmu, delta=delta, dq=dq, update_g=False)
+
+    def get_pressure(self, delta=None, dq=0, mus_eff=None):
+        """return the pressure"""
+        if delta is None:
+            delta = self.delta
+        if mus_eff is None:
+            mu_eff, dmu_eff = self.ff._get_effetive_mus(mu=self.mu, dmu=self.dmu, delta=delta, dq=dq, update_g=False)
+        else:
+            mu_eff, dmu_eff = mus_eff
+        n_a, n_b = self.ff.get_densities(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq)
+        energy_density = self.ff.get_energy_density(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq, n_a=n_a, n_b=n_b)
         mu_a, mu_b = self.mu + self.dmu, self.mu - self.dmu
         pressure = mu_a * n_a + mu_b * n_b - energy_density
         return pressure.n
 
+    def get_current(self, delta=None, dq=0, mus_eff=None):
+        """return the current"""
+        if delta is None:
+            delta = self.delta
+        if mus_eff is None:
+            mu_eff, dmu_eff = self.ff._get_effetive_mus(mu=self.mu, dmu=self.dmu, delta=delta, dq=dq, update_g=False)
+        else:
+            mu_eff, dmu_eff = mus_eff
+        return self.ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq).n
+        
     def SaveToFile(self, data):
         """Save states to persistent storage"""
         currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -143,11 +165,68 @@ class FFStateFinder():
                     break
             self.SaveToFile(rets)
 
+def compute_pressue_current_worker(jsonData_file):
+    jsonData, fileName = jsonData_file
+    filetokens = fileName.split("_")
+    fileName = "FFState_J_P_" + "_".join(filetokens[1:])
+    dim = jsonData['dim']
+    delta = jsonData['delta']
+    mu = jsonData['mu']
+    dmu = jsonData['dmu']
+    data = jsonData['data']
+    ff = FFStateFinder(mu=mu, dmu=dmu, delta=delta, dim=dim, prefix=f"{fileName}_")
+    assert(mus_eff[0]==mu)
+    assert(mus_eff[1]==dmu)
+    output1 = []
+    output2 = []
+    for item in data:
+        dq1, dq2, d = item
+        if dq1 is not None:
+            dic = {}
+            p1 = ff.get_pressure(delta=d, dq=dq1)
+            j1 = ff.get_current(delta=d, dq=dq1)
+            dic['d']=d
+            dic['q']=dq1
+            dic['p']=p1
+            dic['j']=j1
+            output1.append(dic)
+            print(dic)
+        if dq2 is not None:    
+            dic = {}
+            p2 = ff.get_pressure(delta=d, dq=dq2)
+            j2 = ff.get_current(delta=d, dq=dq2)
+            dic['d']=d
+            dic['q']=dq2
+            dic['p']=p2
+            dic['j']=j2
+            output2.append(dic)
+            print(dic)
+    output =[output1, output2]
+    ff.SaveToFile(output)
+    
+def compute_pressue_current(root=None):
+    
+    if root is None:
+        currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        pattern = join(currentdir, "FFState*.json")
+    else:
+        pattern = join(root,"FFState*.json")
+    files = files=glob.glob(pattern)
+
+    jsonObjects = []
+    index = 0;
+    for file in files:
+        if os.path.exists(file):
+            with open(file,'r') as rf:
+                jsonObjects.append((json.load(rf), os.path.splitext(os.path.basename(file))[0]))
+    logic_cpu_count = os.cpu_count() - 1
+    logic_cpu_count = 1 if logic_cpu_count < 1 else logic_cpu_count
+    compute_pressue_current_worker(jsonObjects[0])
+    with Pool(logic_cpu_count) as Pools:
+        Pools.map(compute_pressue_current_worker,jsonObjects)
+
 if __name__ == "__main__":
-    ff = FFStateFinder( dmu=0.16)
-    ff.run(dl=0.001, du=0.2501, dn=300)
-    #logic_cpu_count = os.cpu_count() - 1
-    #logic_cpu_count = 6#1 if logic_cpu_count < 1 else logic_cpu_count
-    #dmus = (0.0)
-    #with Pool(logic_cpu_count) as Pools:
-    #    rets = Pools.map(worker_thread_partial,rs2)
+    compute_pressue_current()
+    #ff = FFStateFinder( dmu=0.16)
+    #ff.run(dl=0.001, du=0.2501, dn=300)
+    

mmf-hfb/mmf_hfb/FuldeFerrelState.py
@@ -47,8 +47,6 @@ class FFState(object):
             self._C = tf.compute_C(mu_a=mu, mu_b=mu, 
                                    delta=delta, q=q, dq=dq, **self._tf_args).n
         self.bSuperfluidity = delta > 0
-        self.mus_eff = self._get_effetive_mus(mu=mu, dmu=dmu, delta=delta, 
-                                            q=q, dq=dq, k_c=k_c, update_g=True)
         
     def f(self, mu, dmu, delta, q=0, dq=0, **kw):
         args = dict(self._tf_args)
@@ -61,18 +59,30 @@ class FFState(object):
     def get_g(self, delta, mu=None, dmu=None, q=0, dq=0, **kw):
         assert (mu is None) == (dmu is None)
         if mu is None:
-            mu, dmu = self.mus_eff
+            mu, dmu = self.mus
         args = dict(self._tf_args, q=q, dq=dq, delta=delta)
         args.update(kw, mu_a=mu+dmu, mu_b=mu-dmu)
         nu_delta = tf.integrate_q(tf.nu_delta_integrand, **args)
         g = 1./nu_delta.n
         return g
+    def get_current(self, mu=None, dmu=None, q=0, dq=0, delta=None, k_c=None):
+        """return the densities of two the components"""
+        assert (mu is None) == (dmu is None)
+        if mu is None:
+            mu, dmu = self.mus
 
+        if delta is None:
+            delta = self.solve(mu=mu, dmu=dmu, q=q, dq=dq, 
+                               a=self.delta * 0.8, b=self.delta * 1.2)
+        mu_a, mu_b = mu + dmu, mu - dmu
+        args = dict(self._tf_args)
+        return tf.compute_current(mu_a=mu_a, mu_b=mu_b, delta=delta, **args)
+        
     def get_densities(self, mu=None, dmu=None, q=0, dq=0, delta=None, k_c=None):
         """return the densities of two the components"""
         assert (mu is None) == (dmu is None)
         if mu is None:
-            mu, dmu = self.mus_eff
+            mu, dmu = self.mus
 
         if delta is None:
             delta = self.solve(mu=mu, dmu=dmu, q=q, dq=dq, 
@@ -207,7 +217,7 @@ class FFState(object):
                            n_a=None, n_b=None, k_c=None):
         assert (mu is None) == (dmu is None)
         if mu is None:
-            mu, dmu = self.mus_eff
+            mu, dmu = self.mus
 
         if delta is None:
             delta = self.solve(mu=mu, dmu=dmu, q=q, dq=dq, 
@@ -234,7 +244,7 @@ class FFState(object):
         if mu is None:
             mu, dmu = self.mus 
         if mu_eff is None:
-            mu_eff, dmu_eff = self.mus_eff
+            mu_eff, dmu_eff = self._get_effetive_mus(mu=mu, dmu=dmu, q=q, dq=dq)
         if delta is None:
             delta = self.solve(mu=mu_eff, dmu=dmu_eff, q=q, dq=dq, 
                                a=self.delta * 0.8, b=self.delta * 1.2)
@@ -254,7 +264,7 @@ class FFState(object):
         """
         assert (mu is None) == (dmu is None)
         if mu is None:
-            mu, dmu = self.mus_eff
+            mu, dmu = self.mus
 
         oldFlag = self.bStateSentinel
         self.bStateSentinel = False
@@ -271,7 +281,7 @@ class FFState(object):
         """
         assert (mu is None) == (dmu is None)
         if mu is None:
-            mu, dmu = self.mus_eff
+            mu, dmu = self.mus
 
         if a is None:
             a = a=self.delta * 0.1

mmf-hfb/mmf_hfb/homogeneous.py
@@ -3,8 +3,7 @@ import warnings
 import numpy as np
 import scipy.integrate
 import scipy as sp
-import math
-from .integrate import quad, dquad
+
 from uncertainties import ufloat
 
 _QUAD_ARGS = dict(
@@ -12,7 +11,7 @@ _QUAD_ARGS = dict(
     epsrel=1.49e-08)
 
 
-def quad_k(f, kF=None, k_0=0, k_inf=np.inf, dim=1, points=None, limit=1000, **kw):
+def quad_k(f, kF=None, k_0=0, k_inf=np.inf, dim=1, limit=1000, **kw):
     """Integrate from k_0 to k_inf in dim-dimensions including factors
     of 1/(2*pi)**dim and spherical area."""
     args = dict(_QUAD_ARGS, **kw)
@@ -34,20 +33,14 @@ def quad_k(f, kF=None, k_0=0, k_inf=np.inf, dim=1, points=None, limit=1000, **kw
     else:
         raise NotImplementedError(f"Only dim=1,2,3 supported (got {dim}).")
 
-    if points is not None:
-        if kF is not None:
-            points.append(kF)
-        res= quad(func=integrand, a=k_0, b=k_inf, points=points, limit=limit) 
+    if kF is None:
+        res = ufloat(*sp.integrate.quad(integrand, k_0, k_inf, **args))
     else:
-        if kF is None:
-            res = ufloat(*sp.integrate.quad(integrand, k_0, k_inf, **args))
-        else:
-            # One might think that `points=[kF]` could be used here, but
-            # this does not work with infinite limits.
-
-            res = (ufloat(*sp.integrate.quad(integrand, k_0, kF, **args))
-                   + ufloat(*sp.integrate.quad(
-                       integrand, kF, k_inf, limit=limit, **args)))
+        # One might think that `points=[kF]` could be used here, but
+        # this does not work with infinite limits.
+        res = (ufloat(*sp.integrate.quad(integrand, k_0, kF, **args))
+               + ufloat(*sp.integrate.quad(
+                   integrand, kF, k_inf, limit=limit, **args)))
 
     if abs(res.s) > 1e-6 and abs(res.s/res.n) > 1e-6:
         warnings.warn(f"Integral did not converge: {res}")
@@ -228,72 +221,14 @@ class Homogeneous(object):
         
         return namedtuple('Densities', ['n_a', 'n_b', 'nu'])(
             n_a, n_b, nu)
-
-    def get_ns(self, mus_eff, delta, N_twist=1):
-        """Return density n_a, n_b only for speed"""
-        kF = np.sqrt(2*max(0, max(mus_eff)))
-        
-        if self.Nxyz is None:
-            def quad(f):
-                # the special points here does get rid of warnings
-                mu = (mus_eff[0] + mus_eff[0])/2
-                dmu = (mus_eff[0] - mus_eff[0])/2
-                m = minv = hbar= 1
-                dq = q= 0
-                mu_q = mu - dq**2/2*minv
-                p_x_special = (np.ma.divide(m*(dmu - np.array([delta, -delta])),
-                                dq).filled(np.nan) -q).tolist()
-
-                P = [1, 0, -4*(m*mu_q + dq**2),
-                     8*m*dq*dmu, 4*m**2*(delta**2 + mu_q**2 - dmu**2)]
-                p_x_special.extend([p.real - q for p in np.roots(P)])
-                points = sorted(set([x/hbar for x in p_x_special if not math.isnan(x)]))
-                return quad_k(f, dim=self.dim, kF=kF, points = points)
-        else:
-            def quad(f):
-                return quad_l(f, Nxyz=self.Nxyz, Lxyz=self.Lxyz,
-                              N_twist=N_twist)
-
-
-        def np_integrand(k):
-            """Density"""
-            res = self.get_res(k=k, mus_eff=mus_eff, delta=delta)
-            f_nu = self.f(res.w_m) - self.f(res.w_p)
-            n_p = 1 - res.e_p/res.E*f_nu
-            return n_p
-
-        def nm_integrand(k):
-            """Density"""
-            res = self.get_res(k=k, mus_eff=mus_eff, delta=delta)
-            n_m = self.f(res.w_p) - self.f(-res.w_m)
-            return n_m
-
-        n_m = quad(nm_integrand)
-        n_p = quad(np_integrand)
-        n_a = (n_p + n_m)/2.0
-        n_b = (n_p - n_m)/2.0
-        return (n_a, n_b)
-
+    
     def get_BCS_v_n_e(self, mus_eff, delta, N_twist=1):
         """Return `(v_0, n, mu, e)` for the 1D BCS solution at T=0."""
         kF = np.sqrt(2*max(0, max(mus_eff)))
         
         if self.Nxyz is None:
             def quad(f):
-                mu = (mus_eff[0] + mus_eff[0])/2
-                dmu = (mus_eff[0] - mus_eff[0])/2
-                m = minv = hbar= 1
-                dq = q= 0
-                mu_q = mu - dq**2/2*minv
-                p_x_special = (np.ma.divide(m*(dmu - np.array([delta, -delta])),
-                                dq).filled(np.nan) -q).tolist()
-
-    # Quartic polynomial for special points.  See Docs/Integrate.ipynb
-                P = [1, 0, -4*(m*mu_q + dq**2),
-                     8*m*dq*dmu, 4*m**2*(delta**2 + mu_q**2 - dmu**2)]
-                p_x_special.extend([p.real - q for p in np.roots(P)])
-                points = sorted(set([x/hbar for x in p_x_special if not math.isnan(x)]))
-                return quad_k(f, dim=self.dim, kF=kF, points = points)
+                return quad_k(f, dim=self.dim, kF=kF)
         else:
             def quad(f):
                 return quad_l(f, Nxyz=self.Nxyz, Lxyz=self.Lxyz,

mmf-hfb/mmf_hfb/tf_completion.py
@@ -143,6 +143,17 @@ def C_integrand(ka2, kb2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
     f_nu = (f(w_m, T) - f(w_p, T))
     return 0.5*(1/e_p - f_nu/E)
 
+@numba.jit(nopython=True)
+def f_p_m(ka2, kb2, mu_a, mu_b, delta, m_a, m_b, hbar, T):
+        e = hbar**2/2
+        e_a, e_b = e*ka2/m_a - mu_a, e*kb2/m_b - mu_b
+        e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
+        E = np.sqrt(e_p**2 + abs(delta)**2)
+        w_m, w_p = e_m - E, e_m + E
+        f_nu = (f(w_m, T) - f(w_p, T))
+        f_p = 1 - e_p/E*f_nu
+        f_m = f(w_p, T) - f(-w_m, T)
+        return (f_p,f_m)
 
 class Series(object):
     """Series expansions for T=0 and large enough k_c."""
@@ -213,6 +224,8 @@ def do_integration(integrand, mu_a, mu_b, delta, m_a, m_b, dim=3,
     mu_q = mu - dq**2/2*minv
     assert m_a == m_b   # Need to re-derive for different masses
     m = 1./minv
+    if limit is None:
+        limit = MAX_DIVISION
     #kF = math.sqrt(2*mu/minv)/hbar
 
     # The following conditions were derived from w(kx, kp) = 0 and
@@ -267,42 +280,33 @@ def compute_current(mu_a, mu_b, delta, m_a=1, m_b=1, dim=3, hbar=1.0, T=0.0,
                     q=0, dq=0, k_0=0, k_c=None):
     """compute the overall current"""
     k_inf = np.inf if k_c is None else k_c
-    def f(ka2, kb2):
-        e = hbar**2/2
-        e_a, e_b = e*ka2/m_a - mu_a, e*kb2/m_b - mu_b
-        e_m, e_p = (e_a - e_b)/2, (e_a + e_b)/2
-        E = np.sqrt(e_p**2 + abs(delta)**2)
-        w_m, w_p = e_m - E, e_m + E
-        f_nu = (f(w_m, T) - f(w_p, T))
-        f_p = 1 - e_p/E*f_nu
-        f_m = f(w_p, T) - f(-w_m, T)
-        return (f_p,f_m)
+    
 
     if dim == 1:
         def integrand(k):
             k2_a = (k + q + dq)**2
             k2_b = (k + q - dq)**2
-            f_p, f_m = f(k2_a, k2_b)
+            f_p, f_m = f_p_m(k2_a, k2_b, mu_a, mu_b, delta, m_a, m_b, hbar, T)
             return (k * f_p + dq * f_m) / 2 / np.pi
     elif dim == 2:
         def integrand(kx, kp):
             # print(kx, kp)
             k2_a = (kx + q + dq)**2 + kp**2
             k2_b = (kx + q - dq)**2 + kp**2
-            f_p, f_m = f(k2_a, k2_b)
-            return (k * f_p + dq * f_m) / (2*np.pi**2)
+            f_p, f_m = f_p_m(k2_a, k2_b, mu_a, mu_b, delta, m_a, m_b, hbar, T)
+            return (kx * f_p + dq * f_m) / (2*np.pi**2)
     elif dim == 3:
         def integrand(kx, kp):
             # print(kx, kp)
             k2_a = (kx + q + dq)**2 + kp**2
             k2_b = (kx + q - dq)**2 + kp**2
-            f_p, f_m = f(k2_a, k2_b)
-            return (k * f_p + dq * f_m) * (kp/4/np.pi**2)
+            f_p, f_m = f_p_m(k2_a, k2_b, mu_a, mu_b, delta, m_a, m_b, hbar, T)
+            return (kx * f_p + dq * f_m) * (kp/4/np.pi**2)
     else:
         raise ValueError(f"Only dim=1, 2, or 3 supported (got dim={dim})")
         
     return do_integration(integrand, delta=delta, mu_a=mu_a, mu_b=mu_b, m_a=m_a, m_b=m_b, 
-                   dim=dim, q=q, dq=dq, hbar=hbar, k_0=k_0, k_inf=k_inf, limit=limit)
+                   dim=dim, q=q, dq=dq, hbar=hbar, k_0=k_0, k_inf=k_inf, limit=None)
 
 def integrate(f, mu_a, mu_b, delta, m_a, m_b, dim=3, hbar=1.0, T=0.0,
               k_0=0.0, k_c=np.inf):
@@ -360,8 +364,6 @@ def integrate_q(f, mu_a, mu_b, delta, m_a, m_b, dim=3,
                          mu_a=mu_a, mu_b=mu_b, delta=delta,
                          m_a=m_a, m_b=m_b, hbar=hbar, T=T)
 
-    if limit is None:
-        limit = MAX_DIVISION
         
     delta = abs(delta)
     args = (mu_a, mu_b, delta, m_a, m_b, hbar, T)