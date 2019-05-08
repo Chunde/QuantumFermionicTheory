BUG, TST, WIP: Restructing the code, fixed wrong formula in the functional code and updated relative code.

mmf-hfb/mmf_hfb/Functionals.py
@@ -1,12 +1,17 @@
 import numpy as np
 
 
-class Functionals(object):
+class FuncionalBase(object):
     m = 1
     hbar = 1
+
+    pass
+
+
+class Functional(FuncionalBase):
     
     def __init__(self):
-        self.gamma = -11.11
+        self.gamma = self._gamma()
 
     def _get_p(self, ns):
         """return p for a given ns"""
@@ -18,48 +23,65 @@ class Functionals(object):
         p = n_m /n_p
         return p
 
-    def _alpha(self, p=0):
-        """return alpha"""
-        "[Numerical Test Status:Pass]"
-        p2, p4 = p**2, p**4
-        alpha = 1.0094 + 0.532 * p2 * (1 - p2 + p4 / 3.0) + 0.156 * p * (1 - 2.0 * p2 / 3.0 + p4 / 5.0)
-        return alpha
-
     def _G(self, p):
         "[Numerical Test Status:Pass]"
-        return 0.357 + 0.642 * p **2
+        return 0.357 + 0.642*p**2
 
     def _dG_dp(self, p):
         "[Numerical Test Status:Pass]"
-        return 1.284 * p
+        return 1.284*p
 
     def _dalpha_dp(self, p):
         """return dalpha / dp"""
         "[Numerical Test Status:Pass]"
         #return -1.064*p**5 + 0.156*p**4 + 2.128*p**3 + 0.312*p**2 - 1.064*p + 0.156 # from Mathematica, wrong
         #return (133*p*(p**4/3 - p**2 + 1))/125 - (39*p*((4*p)/3 - (4*p**3)/5))/250 - (133*p**2*(2*p - (4*p**3)/3))/250 - (13*p**2)/125 + (39*p**4)/1250 + 39/250 # from matlab,right
-        return ((266*p + 39)*(p**2 - 1)**2)/250.0  # from matlab, simplified version, pass the test
+        #return ((266*p + 39)*(p**2 - 1)**2)/250.0  # from matlab, simplified version, pass the test
+        return -((266*p - 39)*(p**2 - 1)**2)/250.0  # from matlab, simplified version, pass the test
 
-    def _gamma(self, p):
+    def _gamma(self, p=None):
         return -11.11
 
-    def _alpha_a(self, n_a, n_b):
-        return self._alpha(self._get_p((n_a, n_b)))
+    def _get_alphas_p(self, p):
+        p2 = p**2
+        p4 = p2**2
+        alpha_even = 1.094 - 0.532*p2*(1 - p2 + p4/3.0)
+        alpha_odd = 0.156*p*(1 - 2.0*p2/3.0 + p4/5.0)
+        alpha_a, alpha_b = alpha_odd + alpha_even, -alpha_odd + alpha_even
+        return (alpha_a, alpha_b, alpha_even, alpha_odd)
+
+    def _alpha(self, p=0):
+        """return alpha"""
+        "[Numerical Test Status:Pass]"
+        alpha_a, alpha_b, alpha_odd, alpha_even = self._get_alphas_p(p)
+        alpha = alpha_odd + alpha_even
+        return alpha
+
+    def _get_alphas(self, ns=None):
+        """return alpha_a, alpha_b, alpha_p"""
+        p = self._get_p(ns)
+        alpha_a, alpha_b, alpha_odd, alpha_even = self._get_alphas_p(p)
+        # alpha_p is defined as (alpha_a + alpha_b) /2 , it's alpha_even
+        alpha_p = alpha_even
+        return (alpha_a, alpha_b, alpha_p)
+
+    def _alpha_a(self, ns):
+        return self._alpha(self._get_p(ns))
      
-    def _alpha_b(self, n_a, n_b):
-        return self._alpha(-self._get_p((n_a, n_b)))
+    def _alpha_b(self, ns):
+        return self._alpha(-self._get_p(ns))
 
     def _alpha_p(self, p):
-        return 0.5 * (self._alpha(p) + self._alpha(-p))
+        return 0.5*(self._alpha(p) + self._alpha(-p))
 
     def _alpha_m(self, p):
-        return 0.5 * (self._alpha(p) - self._alpha(-p))
+        return 0.5*(self._alpha(p) - self._alpha(-p))
 
     def _dalpha_p_dp(self, p):
         """return dalpha_p / dp"""
         "[Matlab verified]"
         "[Numerical Test Status:Pass]"
-        return 1.064*p*(p**2 - 1.0)**2
+        return -1.064*p*(p**2 - 1.0)**2
 
     def _dalpha_m_dp(self, p):
         """return dalpha_m / dp"""
@@ -67,22 +89,22 @@ class Functionals(object):
         "[Numerical Test Status:Pass]"
         return 0.156*(p**2 - 1.0)**2
 
-    def _C(self, n_a, n_b):
+    def _C(self, ns):
         """return C tilde"""
         "[Numerical Test Status:Pass]"
-        p = self._get_p((n_a, n_b))
-        return self._alpha_p(p) * (n_a + n_b)**(1.0/3) / self.gamma
+        p = self._get_p(ns)
+        return self._alpha_p(p)*(sum(ns))**(1.0/3)/self.gamma
 
-    def _D(self, n_a, n_b):
+    def _D(self, ns):
         "[Numerical Test Status:Pass]"
-        N1 = (6 * np.pi**2 *(n_a + n_b))**(5.0/3)/20/np.pi**2
-        p = self._get_p((n_a, n_b))
-        N2 = self._G(p) - self._alpha(p) * ((1+p)/2.0)**(5.0/3) - self._alpha(-p) * ((1-p)/2.0)**(5/3)
-        return N1 * N2
+        N1 = (6*np.pi**2*(sum(ns)))**(5.0/3)/20/np.pi**2
+        p = self._get_p(ns)
+        N2 = self._Dp(p)
+        return N1*N2
 
     def _Dp(self, p):
         "[Numerical Test Status:Pass]"
-        return self._G(p) - self._alpha(p) * ((1+p)/2.0)**(5.0/3) - self._alpha(-p) * ((1-p)/2.0)**(5/3)
+        return self._G(p) - self._alpha(p)*((1+p)/2.0)**(5.0/3) - self._alpha(-p)*((1-p)/2.0)**(5/3)
 
     def _dD_dp(self, p):
         """return the derivative 'dD(p)/dp' """
@@ -95,10 +117,11 @@ class Functionals(object):
         p4 = p3*p
         p5 = p4*p
         p6 = p5*p
-        #dD_p = 1.284*p + 0.57904*p_m + 0.51615*p**2*p_m +0.82315*p**3*p_m - 0.90042*p**4*p_m - 0.40065*p**5*p_m + 0.42823*p**6*p_m - 0.46617*p*p_p - 0.57904*p_p - 0.51615*p**2*p_p + 0.82315*p**3*p_p + 0.90042*p**4*p_p- 0.40065*p**5*p_p - 0.42823*p**6*p_p - 0.46617*p*p_m
-        #dD_p = 1.284*p + (0.57904 - 0.46617*p+ 0.51615*p2 +0.82315*p3 - 0.90042*p4 - 0.40065*p5 + 0.42823*p6)*p_m + ( - 0.57904- 0.46617*p - 0.51615*p2 + 0.82315*p3 + 0.90042*p4- 0.40065*p5 - 0.42823*p6)*p_p
-        #dD_p = 1.284*p - 0.57904 * pm - 0.46617 *p * pp - 0.51615*p2 * pm +0.82315*p3 * pp + 0.90042*p4 * pm - 0.40065*p5 * pp -0.42823*p6*pm
-        dD_p = 1.284*p + (-0.57904 - 0.51615*p2 + 0.90042*p4 -0.42823*p6)*pm +(0.82315*p3 - 0.46617 *p- 0.40065*p5) * pp
+       
+        #dD_p = 1.284*p + 0.62345*p_m - 0.7127*p**2*p_m - 0.51741*p**3*p_m + 0.9987*p**4*p_m + 0.26962*p**5*p_m - 0.42823*p**6*p_m + 0.20411*p*p_p - 0.62345*p_p + 0.7127*p**2*p_p - 0.51741*p**3*p_p - 0.9987*p**4*p_p + 0.26962*p**5*p_p + 0.42823*p**6*p_p + 0.20411*p*p_m
+        #dD_p = 1.284*p + (0.62345 + 0.20411*p - 0.7127*p**2 - 0.51741*p**3 + 0.9987*p**4 + 0.26962*p**5 - 0.42823*p**6)*p_m + (0.20411*p - 0.62345 + 0.7127*p**2 - 0.51741*p**3 - 0.9987*p**4 + 0.26962*p**5 + 0.42823*p**6)*p_p
+        #dD_p = 1.284*p - 0.62345 * pm + 0.20411*p * pp + 0.7127*p**2 * pm - 0.51741*p**3 * pp - 0.9987*p**4 * pm + 0.26962*p**5 * pp + 0.42823*p**6 * pm
+        dD_p = 1.284*p  +(- 0.62345 + 0.7127*p2- 0.9987*p4 + 0.42823*p6) * pm + (0.20411*p  - 0.51741*p3  + 0.26962*p5)*pp 
         return dD_p
 
     def _dD_dn(self, ns):
@@ -116,18 +139,17 @@ class Functionals(object):
         p = self._get_p(ns)
         dp_n_a, dp_n_b = self._dp_dn(ns)
         dD_p = self._dD_dp(p=p)
-        N0 = (6 * np.pi**2) ** (5.0/3) / 20 / np.pi**2
-        N1 = self._D(na, nb) / 0.6 / n
-        N2 = N0 * n**(5/3) * dD_p
-        dD_n_a = N1 + N2 * dp_n_a
-        dD_n_b = N1 + N2 * dp_n_b
+        N0 = (6*np.pi**2)**(5.0/3)/20/np.pi**2
+        N1 = self._D(ns)/0.6/ n
+        N2 = N0*n**(5/3)*dD_p
+        dD_n_a = N1 + N2*dp_n_a
+        dD_n_b = N1 + N2*dp_n_b
         return (dD_n_a, dD_n_b)
 
     def _dC_dn(self, ns):
         """return dC / dn"""
         "[Numerical Test Status:Pass]"
-        na, nb = ns
-        n = na + nb
+        n = sum(ns)
         p = self._get_p(ns)
         dp_n_a, dp_n_b = self._dp_dn(ns)
         dC_dn_a = self._alpha_p(p) * n **(-2/3)/3 + n**(1/3)*self._dalpha_p_dp(p) * dp_n_a
@@ -152,15 +174,4 @@ class Functionals(object):
             Lambda = self.m/self.hbar**2/2/np.pi * np.log((k_c-k0)/(k_c+k0))/k0
         return Lambda
 
-    def _get_alphas(self, ns=None):
-        if ns is None:
-            return (None, None, None)
-        p = self._get_p(ns)
-        p2 = p**2
-        p4 = p2**2
-        alpha_even = 1.0094 + 0.532*p2*(1 - p2 + p4/3.0)
-        alpha_odd =0.156*p* (1 - 2.0*p2/3.0 + p4/5.0)
-        alpha_a, alpha_b = alpha_odd + alpha_even, -alpha_odd + alpha_even
-        # alpha_p is defined as (alpha_a + alpha_b) /2 , it's alpha_even
-        alpha_p = alpha_even
-        return (alpha_a, alpha_b, alpha_p)
+

mmf-hfb/mmf_hfb/bcs.py
@@ -101,7 +101,7 @@ class BCS(object):
                        self.fft(K))).reshape(mat_shape)
         return K
 
-    def get_Ks(self, twists):
+    def get_Ks(self, twists, **kw):
         K = self._get_K(twists)
         return (K, K)
 
@@ -115,7 +115,7 @@ class BCS(object):
             axes = range(self.dim)
         return np.fft.ifftn(y, axes=axes)
 
-    def get_v_ext(self):
+    def get_v_ext(self, **kw):
         """Return the external potential."""
         return (0, 0)
 
@@ -129,7 +129,7 @@ class BCS(object):
             f = (1 - np.sign(E))/2
         return f
 
-    def get_H(self, mus_eff, delta, twists=0):
+    def get_H(self, mus_eff, delta, twists=0, **kw):
         """Return the single-particle Hamiltonian with pairing.
 
         Arguments
@@ -144,8 +144,8 @@ class BCS(object):
         """
         zero = np.zeros_like(sum(self.xyz))
         Delta = np.diag((delta + zero).ravel())
-        K_a, K_b = self.get_Ks(twists=twists)
-        v_a, v_b = self.v_ext
+        K_a, K_b = self.get_Ks(twists=twists, **kw)
+        v_a, v_b = self.get_v_ext(**kw)
         mu_a, mu_b = mus_eff
         mu_a += zero
         mu_b += zero

mmf-hfb/mmf_hfb/vortex_aslda.py
@@ -2,13 +2,13 @@
 This module provides a ASLDA method for solving the polarized
 two-species Fermi gas with short-range interaction.
 """
-from mmf_hfb.Functionals import Functionals
+from mmf_hfb.Functionals import Functional
 from mmfutils.math.integrate import mquad
 from mmf_hfb.bcs import BCS
 import numpy as np
 
 
-class ASLDA(Functionals, BCS):
+class ASLDA(Functional, BCS):
     hbar = 1.0
     m = 1.0
     
@@ -17,7 +17,7 @@ class ASLDA(Functionals, BCS):
         Functionals.hbar, Functionals.m = BCS.hbar, BCS.m
         self.E_c = E_c
         # Second order derivative operator without twisting
-        self._D2 = self._get_K()
+        self._D2 = BCS._get_K(self)
   
     def _get_modified_K(self, D2, alpha):
         """"
@@ -101,7 +101,7 @@ class ASLDA(Functionals, BCS):
         ---------
         k_p: kinetic energy offset added to the diagonal elements
         """
-        K = self._get_K(twists)  # the K already has factor of hbar^2/2m
+        K = BCS._get_K(self, twists)  # the K already has factor of hbar^2/2m
         k_p = np.diag(np.ones_like(sum(self.xyz)) * k_p)
         K = K + k_p
         alpha_a, alpha_b, alpha_p = self._get_alphas(ns)

mmf-hfb/tests/test_aslda_functional.py
@@ -1,9 +1,9 @@
 import numpy as np
-from mmf_hfb.Functionals import Functionals
+from mmf_hfb.Functionals import Functional
 import itertools  
 
 def test_derivative():
-    a = Functionals()
+    a = Functional()
     dx = 1e-6
     nas = np.linspace(0.1,1,10)
     nbs = np.linspace(0.1,1,10)
@@ -16,27 +16,27 @@ def test_derivative():
         p = a._get_p(ns)
 
         # test _dalpha_dp
-        dalpha_p = (a._alpha(p + dx) - a._alpha(p-dx)) / 2 / dx
+        dalpha_p = (a._alpha(p + dx) - a._alpha(p-dx))/2/dx
         dalpha_p_ = a._dalpha_dp(p)
 
         assert np.allclose(dalpha_p, dalpha_p_, atol=0.0005)
 
         # test _get_dD_dp
-        dD_p =   (a._Dp(p + dx) - a._Dp(p-dx))/ 2 / dx
+        dD_p =   (a._Dp(p + dx) - a._Dp(p-dx))/2/dx
         dD_p_ = a._dD_dp(p=p)
         assert np.allclose(dD_p, dD_p_, atol=0.0005)
 
         # test get_dD_dn
-        dD_n_a = (a._D(na + dx,nb)-a._D(na-dx,nb))/2/dx
-        dD_n_b = (a._D(na,nb + dx)-a._D(na,nb - dx))/2/dx
+        dD_n_a = (a._D((na + dx,nb))-a._D((na-dx,nb)))/2/dx
+        dD_n_b = (a._D((na,nb + dx))-a._D((na,nb - dx)))/2/dx
         dD_n_a_, dD_n_b_ = a._dD_dn(ns)
 
         assert np.allclose(dD_n_a, dD_n_a_, atol=0.0005)
         assert np.allclose(dD_n_b, dD_n_b_, atol=0.0005)
 
         # test _dC_dn
-        dC_n_a = (a._C(na + dx,nb)-a._C(na-dx,nb))/2/dx
-        dC_n_b = (a._C(na,nb + dx)-a._C(na,nb - dx))/2/dx
+        dC_n_a = (a._C((na + dx,nb))-a._C((na-dx,nb)))/2/dx
+        dC_n_b = (a._C((na,nb + dx))-a._C((na,nb - dx)))/2/dx
         dC_n_a_, dC_n_b_ = a._dC_dn(ns)
         assert np.allclose(dC_n_a, dC_n_a_, atol=0.0005)
         assert np.allclose(dC_n_b, dC_n_b_, atol=0.0005)