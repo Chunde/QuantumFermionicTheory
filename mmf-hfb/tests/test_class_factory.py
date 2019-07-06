e92eacc5dba606962a67139de31620813b0478aa
Chunde <xmuhcd@msn.com>
2019-07-05 23:50:20 -0700

    WIP, TST: Update a test file. BCS_aslda code not fully work, need to check when come to vortex

--- mmf-hfb/tests/test_class_factory.py
+++ mmf-hfb/tests/test_class_factory.py
@@ -1,40 +1,60 @@
 from mmf_hfb.ClassFactory import ClassFactory, FunctionalType, KernelType, Solvers
 import numpy as np
+import pytest
+import warnings
+warnings.filterwarnings("ignore")
 
 
-if __name__ == "__main__":
+@pytest.fixture(params=[FunctionalType.ASLDA, FunctionalType.BDG, FunctionalType.SLDA])
+def functional(request):
+    return request.param
+
+@pytest.fixture(params=[KernelType.HOM])
+def kernel(request):
+    return request.param
+
+@pytest.fixture(params=[1, np.pi, 10])
+def mu(request):
+    return request.param
+
+
+def test_class_factory(functional, kernel, mu):
     dx = 1e-3
     L = 0.46
-    N = 16
-    N_twist = 4
+    N = 8
+    N_twist = 1
     delta = 1.0
-    mu=np.pi
+    mu=mu
     dmu = 0
     LDA = ClassFactory(
         className="LDA",
-        functionalType=FunctionalType.BDG,
-        kernelType=KernelType.HOM)
+        functionalType=functional,
+        kernelType=kernel)
 
     lda = LDA(
         Nxyz=(N, ), Lxyz=(L,), mu_eff=mu, dmu_eff=dmu,
         delta=delta, T=0, dim=3)
-    
+
     def get_ns_e_p(mu, dmu, update_C=False):
         ns, e, p = lda.get_ns_e_p(
             mus=(mu, dmu), delta=delta, N_twist=N_twist, Laplacian_only=True,
             update_C=update_C, max_iter=32, solver=Solvers.BROYDEN1)
         return ns, e, p
 
-    ns, e, p = get_ns_e_p(mu=mu, dmu=dmu, update_C=True)
+    lda.fix_C(mu=mu, dmu=dmu, delta=delta)
+    ns, _, _ = get_ns_e_p(mu=mu+dx, dmu=dmu)
     print("-------------------------------------")
     ns1, e1, p1 = get_ns_e_p(mu=mu+dx, dmu=dmu)
     print("-------------------------------------")
     ns2, e2, p2 = get_ns_e_p(mu=mu-dx, dmu=dmu)
-    #print(p1, p2, e1, e2, ns1, ns2)
     n_p = (p1-p2)/2.0/dx
     mu_ = (e1-e2)/(sum(ns1) - sum(ns2))
     print(np.max(n_p), np.max(sum(ns)))
     print(np.max(mu_), mu)
     print("-------------------------------------")
     assert np.allclose(np.max(n_p).real, sum(ns), rtol=1e-2)
-    assert np.allclose(np.max(mu_).real, mu, rtol=1e-2)
+    assert np.allclose(np.mean(mu_).real, mu, rtol=1e-2)
+
+
+if __name__ == "__main__":
+    test_class_factory(functional=FunctionalType.SLDA, kernel=KernelType.HOM, mu=1)
