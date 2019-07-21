BUG, TST, WIP: Fixed a sign bug when compute effective mus(mu_a_eff = mu_a - V_a,  NOT mu_a_ef = mu_a + V_a)

mmf-hfb/mmf_hfb/ClassFactory.py
@@ -86,7 +86,7 @@ class Adapter(object):
         res = self.get_densities(mus_eff=(mu_a_eff, mu_b_eff), delta=delta, **args)
         ns, taus, nu = (res.n_a, res.n_b), (res.tau_a, res.tau_b), res.nu
         V_a, V_b = self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu)
-        mu_a, mu_b = mu_a_eff - V_a, mu_b_eff - V_b
+        mu_a, mu_b = mu_a_eff + V_a, mu_b_eff + V_b
         return (mu_a, mu_b)
 
     def get_mus_eff(self, mus, delta, dq=0, ns=None, taus=None, nu=None, verbosity=True):
@@ -97,7 +97,7 @@ class Adapter(object):
         """
         mu_a, mu_b = mus[0]+mus[1], mus[0]-mus[1]
         V_a, V_b = self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu)
-        mu_a_eff, mu_b_eff = mu_a + V_a, mu_b + V_b
+        mu_a_eff, mu_b_eff = mu_a - V_a, mu_b - V_b
         x0 = np.array([mu_a_eff, mu_b_eff])
 
         def _fun(x):
@@ -105,7 +105,7 @@ class Adapter(object):
             res = self.get_densities(mus_eff=(mu_a_eff, mu_b_eff), delta=delta, dq=dq)
             ns, taus, nu = (res.n_a, res.n_b), (res.tau_a, res.tau_b), res.nu
             V_a, V_b = self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu)
-            mu_a_eff_, mu_b_eff_ = mu_a + V_a, mu_b + V_b
+            mu_a_eff_, mu_b_eff_ = mu_a - V_a, mu_b - V_b
             if not ((mu_a_eff_ > 0) and (mu_b_eff_ > 0)):  # assume positive
                 raise ValueError(f"Effective mu must be positive:{mu_a_eff_, mu_b_eff_}")
             x_ = np.array([mu_a_eff_, mu_b_eff_])
@@ -187,7 +187,7 @@ class Adapter(object):
             ns, taus, nu = (res.n_a, res.n_b), (res.tau_a, res.tau_b), res.nu
             args.update(ns=ns)
             V_a, V_b = self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu)
-            mu_a_eff_, mu_b_eff_ = mu_a + V_a, mu_b + V_b
+            mu_a_eff_, mu_b_eff_ = mu_a - V_a, mu_b - V_b
             g_eff = self._g_eff(mus_eff=(mu_a_eff_, mu_b_eff_), **args)
             delta_  = delta if fix_delta else g_eff*nu
             if verbosity:
@@ -254,7 +254,7 @@ class Adapter(object):
             energy_density = (
                 energy_density
                 +self.T*self.get_entropy(mus_eff=(mu_a_eff, mu_b_eff), delta=delta).n)
-        energy_density = energy_density - D
+        energy_density = energy_density + D
         pressure = ns[0]*mu_a + ns[1]*mu_b - energy_density
         return (ns, energy_density, pressure)
 
@@ -276,7 +276,7 @@ class Adapter(object):
         res = self.get_densities(mus_eff=(mu_a_eff, mu_b_eff), delta=delta, **args)
         ns, taus, nu = (res.n_a, res.n_b), (res.tau_a, res.tau_b), res.nu
         V_a, V_b = self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu)
-        mu_a, mu_b = mu_a_eff - V_a, mu_b_eff - V_b
+        mu_a, mu_b = mu_a_eff + V_a, mu_b_eff + V_b
         D = self.get_D(ns=ns)
         g_eff = 0 if nu==0 else delta/nu
         energy_density = taus[0]/2.0 + taus[1]/2.0 + g_eff*abs(nu)**2
@@ -284,7 +284,7 @@ class Adapter(object):
             energy_density = (
                 energy_density
                 +self.T*self.get_entropy(mus_eff=(mu_a_eff, mu_b_eff), delta=delta).n)
-        energy_density = energy_density - D
+        energy_density = energy_density + D
         pressure = ns[0]*mu_a + ns[1]*mu_b - energy_density
         return (ns, (mu_a, mu_b), energy_density, pressure)
 

mmf-hfb/mmf_hfb/FFStateAgent.py
@@ -1,4 +1,4 @@
-from mmf_hfb.ClassFactory import ClassFactory, FunctionalType, KernelType
+from mmf_hfb.ClassFactory import ClassFactory, FunctionalType, KernelType, Solvers
 from mmf_hfb.DataHelper import ff_state_sort_data
 from mmf_hfb.ParallelHelper import PoolHelper
 from scipy.optimize import brentq
@@ -20,7 +20,7 @@ class FFStateAgent(object):
     def __init__(
             self, mu_eff, dmu_eff, delta, dim, verbosity=True,
             prefix="FFState_", timeStamp=True, **args):
-        assert dmu_eff < delta
+        # assert dmu_eff < delta
         self.delta = delta
         self.mu_eff = mu_eff
         self.dmu_eff = dmu_eff
@@ -36,6 +36,17 @@ class FFStateAgent(object):
         currentdir = os.path.dirname(
             os.path.abspath(inspect.getfile(inspect.currentframe())))
         return join(currentdir, "data", self.fileName)
+
+    def get_other_pressures(self, mus, delta, dq, C, mus_eff):
+        self.C = C
+        # P_ff = self.get_ns_e_p(
+        #     mus=mus, delta=delta, dq=dq, verbosity=False,
+        #     x0=mus_eff +(delta,), solver=Solvers.BROYDEN1)
+        P_ss = self.get_ns_e_p(
+            mus=mus, delta=None, verbosity=False, solver=Solvers.BROYDEN1)
+        P_ns = self.get_ns_e_p(
+            mus=mus, delta=0, verbosity=False, solver=Solvers.BROYDEN1)
+        return (P_ss[2], P_ns[2])
 
     def get_pressure(self, mus_eff, delta, q=0, dq=0):
         """return the pressure only"""
@@ -371,9 +382,9 @@ def LabelStates(current_dir=None, raw_data=False, verbosity=False):
     can be used for plotting.
     Para:
     --------------
-    current_dir: specifies the dir where to find the 
+    current_dir: specifies the dir where to find the
         pressure and current output files
-    raw_data: if this is True, the original data will 
+    raw_data: if this is True, the original data will
         also be included in the return list items
 
     """
@@ -386,12 +397,14 @@ def LabelStates(current_dir=None, raw_data=False, verbosity=False):
     output = []
     pattern = join(current_dir, "FFState_J_P[()d_0-9]*")
     files=glob.glob(pattern)
+
     for file in files[0:]:
         if os.path.exists(file):
             with open(file, 'r') as rf:
                 ret = json.load(rf)
                 dim, mu_eff, dmu_eff, delta, C=(
                     ret['dim'], ret['mu_eff'], ret['dmu_eff'], ret['delta'], ret['C'])
+                
                 p0 = ret['p0']
                 a_inv = 4.0*np.pi*C  # inverse scattering length
 
@@ -420,13 +433,30 @@ def LabelStates(current_dir=None, raw_data=False, verbosity=False):
                     n_a, n_b = data['na'], data['nb']
                     mu_a, mu_b = data['mu_a'], data['mu_b']
                     mu, dmu = (mu_a + mu_b)/2.0, (mu_a - mu_b)/2.0
+                    #  create a lda instance to compute all types of pressure
+                    args = dict(
+                        mu_eff=mu_eff, dmu_eff=dmu_eff, delta=delta,
+                        T=0, dim=ret['dim'], k_c=ret['k_c'], verbosity=False, C=C)
+                    lda = ClassFactory(
+                        "LDA", (FFStateAgent,),
+                        functionalType=FunctionalType.ASLDA,
+                        kernelType=KernelType.HOM, args=args)
+                    
                     if verbosity:
                         print(f"na={n_a}, nb={n_b}, PF={value}, PN={p0}")
-                    if (value > p0) and (
+                    if (
                         not np.allclose(
                             n_a, n_b, rtol=1e-9) and (
                                 data["q"]>0.0001 and data["d"]>0.001)):
-                        bFFState = True
+                        try:
+                            pressures = lda.get_other_pressures(
+                                mus_eff=(mu_eff + dmu_eff, mu_eff - dmu_eff),
+                                mus=(mu, dmu), delta=data["d"], dq=data['q'], C=C)
+                        except:
+                            print(file)
+                            break
+                        if data['p']>pressures[0] and data['p']>pressures[1]:
+                            bFFState = True
                 if bFFState and verbosity:
                     print(f"FFState: {bFFState} |<-------------")
                 dic = dict(

mmf-hfb/mmf_hfb/homogeneous_aslda.py
@@ -55,8 +55,9 @@ class BDG(Homogeneous, FunctionalBdG):
                 mu_a_eff, mu_b_eff, delta = x
                 res = self.get_densities(mus_eff=(mu_a_eff, mu_b_eff), delta=delta)
                 ns, taus, nu = (res.n_a, res.n_b), (res.tau_a, res.tau_b), res.nu
-                mu_a_eff_, mu_b_eff_ = (np.array([mu_a, mu_b])
-                    + self.get_Vs(delta=delta, ns=ns, taus=taus, nu=nu))
+                mu_a_eff_, mu_b_eff_ = (
+                    np.array([mu_a, mu_b] - self.get_Vs(
+                        delta=delta, ns=ns, taus=taus, nu=nu)))
                 g_eff = self._g_eff(
                     mus_eff=(mu_a_eff_, mu_b_eff_), ns=ns,
                     dim=self.dim, E_c=self.k_c**2/2/self.m)
@@ -114,7 +115,7 @@ class BDG(Homogeneous, FunctionalBdG):
             energy_density = (
                 energy_density
                 + self.T*self.get_entropy(mus_eff=(mu_a_eff, mu_b_eff), delta=delta).n)
-        energy_density = energy_density - D
+        energy_density = energy_density  + D
         pressure = ns[0]*mu_a + ns[1]*mu_b - energy_density
         if update_C:
             self.C = self.get_C(ns)
@@ -128,7 +129,12 @@ class SLDA(BDG, FunctionalSLDA):
             return (1.0, 1.0)
         elif d==1:
             return (0, 0, 0, 0)
-    
+
+    # def get_D(self, ns, d=0):
+    #     if d==0:
+    #         return 1
+    #     elif d==1:
+    #         return (0, 0)
 
 class ASLDA(SLDA, FunctionalASLDA):
     pass