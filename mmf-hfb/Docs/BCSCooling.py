WIP, TST, BUG, EHN: Added a function to check the dE/dt, it shows that the derivative term indeed was wrong, and need to be fixed. other cooling potential works as expected, i.g. yield negative dE/dt.

mmf-hfb/Docs/BCSCooling.py
@@ -20,6 +20,47 @@ import matplotlib.pyplot as plt
 from nbimports import *
 import numpy as np
 
+# # Departure from Locality
+# We can generalize these operators slightly to cool in a non-local fashion.  For example, consider the following cooling Hamiltonian.  (The motivation here is that the operators $\op{D}$ are derivatives, so this Hamiltonian is quasi-local.)
+#
+# $$
+#   \op{H}_c = \int \d{x}\; \op{D}_a^\dagger\ket{x}V_{ab}(x)\bra{x}\op{D}_b + \text{h.c.},\\
+#   \hbar \dot{E} = -\I\left(
+#     \int\d{x}\;V_{ab}(x)
+#     \braket{x|\op{D}_b[\op{R},\op{H}]\op{D}_a^\dagger|x}
+#     + \text{h.c.}
+#   \right).
+# $$
+#
+# We can ensure cooling if we take:
+#
+# $$
+#   \hbar V_{ab}(x) 
+#   = (\I\hbar\braket{x|\op{D}_b[\op{R},\op{H}]\op{D}_a^\dagger|x})^*
+#   = \I\hbar\braket{x|\op{D}_a[\op{R},\op{H}]\op{D}_b^\dagger|x}\\
+#   = \braket{x|\op{D}_a|\psi}\braket{x|\op{D}_b|\dot{\psi}}^*
+#   + \braket{x|\op{D}_a|\dot{\psi}}\braket{x|\op{D}_b|\psi}^*.
+# $$
+#
+# If $\op{D}_{a,b}(x)$ are just derivative operators $\braket{x|\op{D}_{a}|\psi} = \psi^{(a)}(x)$, then we have
+#
+# $$
+#   \hbar V_{ab}(x) 
+#   = \psi^{(a)}(x)\overline{\dot{\psi}^{(b)}(x)}
+#   + \dot{\psi}^{(a)}(x)\overline{\psi^{(b)}(x)},
+# $$
+#
+# where
+#
+# $$
+#   \dot{\psi}(x) = -\I\hbar\braket{x|\op{H}|\psi}
+# $$
+#
+# is the time-derivative with respect to the original Hamiltonian.  Note that these "potentials" are no longer diagonal in either momentum or position space, so they should be implemented in the usual fashion with an integrator like ABM.
+# $$
+#   \I\hbar \dot{\psi}(x) = \cdots  - \pdiff{}{x}\left(V_{11}(x) \pdiff{}{x}\psi\right)
+# $$
+
 # + {"id": "ptb73kVS8ceS", "colab_type": "text", "cell_type": "markdown"}
 # # BCS Cooling Class Test
 # * To properly display equations, we define some command to make life easier, this commands are invisible
@@ -101,11 +142,9 @@ def check_uv_ir_error(psi, plot=False):
 #
 # It's found that $V_{ab}=0$, since any wave function can be expanded as plane wave, that means for any wave function, as long as the Hamiltonian is free-partile type, $V_{ab}=0$
 
-def test_der_cooling(psi = None, evolve=False, T=0.5, **args):
+def test_der_cooling(psi = None, evolve=True, T=0.5, **args):
     b = BCSCooling(**args)
-    print(b.E_max)
-    da, db=b.divs
-    
+    da, db=b.divs    
     k0 = 2*np.pi/b.L
     x = b.xyz[0]
     V = x**2/2
@@ -118,7 +157,7 @@ def test_der_cooling(psi = None, evolve=False, T=0.5, **args):
         psi = U0[1]  # np.exp(1j*n*(k0*x))
     psi_a = b.Del(psi, n=da)
     Hpsi = np.array(b.apply_H([psi], V=V))[0]/(1j)
-    plt.figure(figsize=(18,6))
+    plt.figure(figsize=(18, 6))
     N = 4 if evolve else 2   
     plt.subplot(1,N,1)
     plt.plot(x, abs(Hpsi),'-', label=r'$H\psi$')
@@ -152,8 +191,15 @@ def test_der_cooling(psi = None, evolve=False, T=0.5, **args):
     return psis[0][-1]
 
 
-args = dict(N=128, dx=0.1, beta_0=1, divs=(0, 1), beta_K=1, beta_V=0, T=5, beta_D=0, evolve=True)
+args = dict(N=128, dx=0.1, divs=(0, 1), beta_K=1, beta_V=1, T=5, beta_D=0, check_dE=True)
 psi = test_der_cooling(**args)
+
+# \begin{align}
+#   \dot{E} &= \bra{\dot{\psi}}\pdiff{E}{\bra{\psi}} + \pdiff{E}{\ket{\psi}}\ket{\dot{\psi}}
+#            = \braket{\dot{\psi}|\op{H}|\psi} + \braket{\psi|\op{H}|\dot{\psi}}
+#            = \frac{-\braket{\psi|\op{H}_c\op{H}|\psi} + \braket{\psi|\op{H}\op{H}_c|\psi}}{\I\hbar}\\
+#           &= \frac{\braket{\psi|[\op{H},\op{H}_c]|\psi}}{\I\hbar}.
+# \end{align}
 
 # ## Dyadic Cooling
 # To minimize the communication costs, we consider approximating $\op{H}_c$ by a set of dyads:
@@ -420,23 +466,24 @@ plt.legend()
 
 b = BCSCooling(N=64, dx=0.1, beta_0=1, beta_V=1, delta=1, mus=(2, 2))
 x = b.xyz[0]
-V = x**2/2
-H0 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(0, 0))
-H1 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(V, V))
+V0 = x**2/3
+V1 = x**2/2
+H0 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(V0, V0))
+H1 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(V1, V1))
 U0, Es0 = b.get_U_E(H0, transpose=True)
 U1, Es1 = b.get_U_E(H1, transpose=True)
 N_state = 1
 psi0 = U1[64]
 psi = U0[64]
 plt.plot(psi0)
-E0, N0 = b.get_E_Ns(psis=[U1[64]], V=V)
+E0, N0 = b.get_E_Ns(psis=[U1[64]], V=V1)
 psis = [psi]
-for i in range(1):
-    psis = b.step(psis=psis, n=10, V=V)
+for i in range(20):
+    psis = b.step(psis=psis, n=10, V=V1)
     plt.plot(psis[0],'--')
     plt.plot(psi0,'-')
     #print(psis[0].real)
-    E, N = b.get_E_Ns(psis=psis, V=V)
+    E, N = b.get_E_Ns(psis=psis, V=V1)
     plt.title(f"E0={E0.real},E={E.real}")
     plt.show()
     clear_output(wait=True)

mmf-hfb/mmf_hfb/BCSCooling.py
@@ -15,6 +15,10 @@ def Normalize(psi):
     return psi/psi.dot(psi.conj())**0.5
 
 
+class HamiltonianC(object):
+    pass
+
+
 class BCSCooling(BCS):
     """
     1d Local Quantum Friction class
@@ -23,7 +27,7 @@ class BCSCooling(BCS):
     def __init__(
             self, N=256, L=None, dx=0.1, delta=0, mus=(0, 0),
             beta_0=1.0, beta_V=1.0, beta_K=1.0, beta_D=1.0,
-            dt_Emax=1.0, g=0, divs=None, **args):
+            dt_Emax=1.0, g=0, divs=None, check_dE=True, **args):
         """
         Arguments
         ---------
@@ -50,6 +54,7 @@ class BCSCooling(BCS):
         self.beta_D = beta_D
         self.g = g
         self.divs = divs
+        self.check_dE = check_dE
         self._K2 = (self.hbar*np.array(self.kxyz[0]))**2/2/self.m
         self.dt = dt_Emax*self.hbar/self._K2.max()
         self.E_max = self._K2.max()
@@ -259,7 +264,7 @@ class BCSCooling(BCS):
             NOTE: This may not be unitary
         """
         Vmn = self.beta_D*self.get_Vd(psis=psis, V=V)
-        V11_psis = [-1*self.Del(Vmn*self.Del(psi=psi)) for psi in psis]
+        V11_psis = [self.Del(Vmn*self.Del(psi=psi)) for psi in psis]
         return V11_psis
 
     def _apply_expK(self, psi, V, Kc, factor=1):
@@ -299,20 +304,33 @@ class BCSCooling(BCS):
             psis[i] = self._apply_expV(psi=psi, V=V_eff, Vc=Vc, factor=factor)
         return psis
     
-    def apply_Hc(self, psi, V):
+    def apply_Hc(self, psis, V):
         """
         Apply the cooling Hamiltonian.
         or, compute dy/dt w.r.t to Hc
         """
-        H_psi = self.apply_H(psis=[psi], V=V)[0] if self.beta_0 !=0 else 0
-        Vc_psi = self.get_Vc(psis=[psi], V=V)*psi
-        Kc_psi = (
-            self.ifft(self.get_Kc([psi], V=V)*self.fft(psi))
-            if self.beta_K !=0 else 0)
-        Vd_psi = self.apply_Vd(psis=[psi], V=V)[0]  # apply the V11 on psi
-        return (
-            self.beta_0*H_psi + self.beta_V*Vc_psi
-            + self.beta_K*Kc_psi + self.beta_D*Vd_psi)
+        Hc_psis = []
+        H_psis = self.apply_H(psis=psis, V=V) if self.beta_0 != 0 else 0
+        Vc = self.get_Vc(psis=psis, V=V) if self.beta_V !=0 else 0
+        Kc = self.get_Kc(psis, V=V) if self.beta_K !=0 else 0
+        Vd_psis = self.apply_Vd(psis=psis, V=V) if self.beta_D !=0 else (0,)*len(psis)
+        for i, psi in enumerate(psis):
+            Vc_psi = Vc*psi
+            Kc_psi = self.ifft(Kc*self.fft(psi))
+            Hc_psi = (
+                self.beta_0*H_psis[i] + self.beta_V*Vc_psi
+                +self.beta_K*Kc_psi + self.beta_D*Vd_psis[i])
+            Hc_psis.append(Hc_psi)
+        return Hc_psis
+
+    def check_dE_dt(self, psis, V):
+        """compute dE/dt"""
+        H_psis = self.apply_H(psis, V=V)
+        Hc_psis = self.apply_Hc(psis=psis, V=V)
+        dE_dt = sum(
+            [H_psi.conj().dot(Hc_psi)- Hc_psi.conj().dot(H_psi)
+                for (H_psi, Hc_psi) in zip(H_psis, Hc_psis)])/(1j)
+        return dE_dt < 0
 
     def step(self, psis, V, n=1):
         """
@@ -328,7 +346,9 @@ class BCSCooling(BCS):
 
     def compute_dy_dt(self, t, psi, subtract_mu=True):
         """Return dy/dt for ODE integration."""
-        Hpsi = self.apply_Hc(psi, V=self.V)
+        if self.check_dE:
+            assert self.check_dE_dt(psis=[psi], V=self.V)
+        Hpsi = self.apply_Hc([psi], V=self.V)[0]
         if subtract_mu:
             Hpsi -= psi.conj().dot(Hpsi)/psi.dot(psi.conj())*psi
         return Hpsi/(1j*self.hbar)
@@ -382,24 +402,13 @@ class BCSCooling(BCS):
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    b = BCSCooling(N=64, dx=0.1, beta_0=1, beta_V=1, delta=1, mus=(2, 2))
+    b = BCSCooling(N=64, dx=0.1, beta_0=1, beta_V=1, beta_K=1, delta=0, beta_D=0, divs=(1, 1))
     x = b.xyz[0]
     V = x**2/2
-    H0 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(0, 0))
-    H1 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(V, V))
+    H0 = b._get_H(mu_eff=0, V=0)
+    H1 = b._get_H(mu_eff=0, V=V)
     U0, Es0 = b.get_U_E(H0, transpose=True)
     U1, Es1 = b.get_U_E(H1, transpose=True)
-    N_state = 1
-    psi0 = U1[64]
-    psi = U0[64]
-    plt.plot(psi0)
-    E0, N0 = b.get_E_Ns(psis=[U1[64]], V=V)
-    psis = [psi]
-    for i in range(1):
-        E, N = b.get_E_Ns(psis=psis, V=V)
-        psis = b.step(psis=psis, n=10, V=V)
-        plt.plot(psis[0], '--')
-        plt.plot(psi0, '-')
-        plt.title(f"E0={E0.real},E={E.real}")
-        plt.show()
+    psi0 = U1[:1]
+    psis = U0[:1]
+    b.check_dE_dt(psis=psis, V=V)

mmf-hfb/tests/test_BCS_Cooling.py
@@ -79,7 +79,6 @@ def test_apply_Vs(N_state=2):
     V = x**2/2
     H0 = b._get_H(mu_eff=0, V=0)
     U0, E0 = b.get_U_E(H0, transpose=True)
-
     psis = U0[:N_state]
 
     def get_kc(psis, fun):
@@ -108,6 +107,79 @@ def test_apply_Vs(N_state=2):
     vc2 = get_Vc(psis, b.apply_H)
     assert np.allclose(vc1, vc2)
 
+
+def test_der_cooling(psi=None, evolve=True, T=0.5, **args):
+    import matplotlib.pyplot as plt
+    b = BCSCooling(**args)
+    da, db=b.divs
+    k0 = 2*np.pi/b.L
+    x = b.xyz[0]
+    V = x**2/2
+    H0 = b._get_H(mu_eff=0, V=0)
+    H1 = b._get_H(mu_eff=0, V=V)
+    U0, E0 = b.get_U_E(H0, transpose=True)
+    U1, E1 = b.get_U_E(H1, transpose=True)
+    psi_1 = Normalize(np.cos(k0*x))
+    if psi is None:
+        psi = U0[1]  # np.exp(1j*n*(k0*x))
+    psi_a = b.Del(psi, n=da)
+    Hpsi = np.array(b.apply_H([psi], V=V))[0]/(1j)
+    plt.figure(figsize=(18, 6))
+    N = 4 if evolve else 2
+    plt.subplot(1, N, 1)
+    plt.plot(x, abs(Hpsi), '-', label=r'$H\psi$')
+    plt.legend()
+    plt.subplot(1, N, 2)
+    Hpsi_a = b.Del(Hpsi, n=da)
+    if da == db:
+        psi_b = psi_a
+        Hpsi_b = Hpsi_a
+    else:
+        psi_b = b.Del(psi, n=db)
+        Hpsi_b = b.Del(Hpsi, n=db)
+    Vc = psi_a*Hpsi_b.conj() + Hpsi_a*psi_b.conj()
+    plt.plot(x, Vc, label='real')
+    plt.plot(x, Vc.imag, label='imag')
+    plt.plot(x, abs(Vc), label='|Vc|')
+    plt.legend()
+    if evolve:
+        b.erase_max_ks()
+        plt.subplot(1, N, 3)
+        ts, psis = b.solve([psi], T=T, rtol=1e-5, atol=1e-6, V=V, method='BDF', check_dE=True)
+        psi0 = U1[0]
+        E0, _ = b.get_E_Ns([psi0], V=V)
+        Es = [b.get_E_Ns([_psi], V=V)[0] for _psi in psis[0]]
+        plt.plot(x, Prob(psis[0][0]), "+", label='init')
+        plt.plot(x, Prob(psis[0][-1]), '--', label="final")
+        plt.plot(x, Prob(U1[0]), label='Ground')
+        plt.legend()
+        plt.subplot(1, N, 4)
+        plt.plot(ts[0][:-2], (Es[:-2] - E0)/abs(E0))
+    return psis[0][-1]
+
+def pairing_cooling():
+    import matplotlib.pyplot as plt
+    b = BCSCooling(N=64, dx=0.1, beta_0=1, beta_V=1, delta=1, mus=(2, 2))
+    x = b.xyz[0]
+    V = x**2/2
+    H0 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(0, 0))
+    H1 = b.get_H(mus_eff=b.mus, delta=b.delta, Vs=(V, V))
+    U0, Es0 = b.get_U_E(H0, transpose=True)
+    U1, Es1 = b.get_U_E(H1, transpose=True)
+    psi0 = U1[64]
+    psi = U0[64]
+    plt.plot(psi0)
+    E0, N0 = b.get_E_Ns(psis=[U1[64]], V=V)
+    psis = [psi]
+    for i in range(1):
+        E, N = b.get_E_Ns(psis=psis, V=V)
+        psis = b.step(psis=psis, n=10, V=V)
+        plt.plot(psis[0], '--')
+        plt.plot(psi0, '-')
+        plt.title(f"E0={E0.real},E={E.real}")
+        plt.show()
+
+
 if __name__ == "__main__":
-    test_derivative_cooling(n=3, da=3, db=2)
-    test_apply_Vs()
+    args = dict(N=128, dx=0.1, beta_0=1, divs=(1, 1), beta_K=1, beta_V=1, beta_D=0)
+    psi = test_der_cooling(**args)