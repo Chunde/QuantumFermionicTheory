7c98cae21ef90680a9060c8e96b3039ab119185b
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2020-03-13 13:52:11 -0700

    WIP: Moved all interfaces into interfaces.py and use zope.interface.

--- mmf-hfb/README.md
+++ mmf-hfb/README.md
@@ -2,5 +2,17 @@
 
 This project can be found at https://cocalc.com/projects/89f60b3e-35bd-473e-b0c4-b00d176b9ff8/files/repositories/mmf-hfb/
 
+# Structure
+
+`_ext`: External resources not managed under version control.  For
+   example, if we depend on the sources of another project, we will
+   clone it here and create appropriate symlinks to the project.  (Tag
+   the revision numbers in the `.mrfreeze` file or elsewhere as
+   appropriate.
+`docs`: Documentation.
+`mmf_hfb`: Python source code.
+`tests`: Testing.
+
+
 
 
--- mmf-hfb/mmf_hfb/interfaces.py
+++ mmf-hfb/mmf_hfb/interfaces.py
@@ -1,6 +1,5 @@
-"""Interfaces for BEC codes.
+"""Interfaces for the HFB code.
 """
-
 from zope.interface import Attribute, Interface, implementer
 
 
@@ -12,3 +11,80 @@ class IHFB(Interface):
     """Interface for HFB-like codes."""
     basis = Attribute("IBasis instance.")
     functional = Attribute("IFunctional")
+
+    def get_densities(mus_eff, delta, **args):
+        """
+        return the densities(may include ns, taus, js, nu)
+        ----------------
+        mus_eff: the effective chemical potential(mu_a, mu_b)
+        delta: the gap or coupling strength
+        args: other parameters
+        """
+
+    def get_ns_e_p(mus, delta, **args):
+        """
+        return the particle densities(n_a, n_b)
+        energy density and pressure
+        """
+
+
+class IFunctional(Interface):
+    """
+    Interface for functional
+    Note: Interface is unnecessary for Python
+    because python function call is made by looking up
+    a table with based on base classes order
+    """
+    def get_alpha(p, d=0):
+        """
+        Parameters
+        ----------
+        p: polarization in range of (0, 1)
+        d: order of derivative(-1, 0, 1)
+        """
+
+    def get_alphas(ns, d=0):
+        """
+        Parameters
+        ----------
+        ns: densities (na, nb)
+        d: order of derivative
+        Note: call the get_alpha
+        """
+
+    def get_p(ns, d=0):
+        """
+        Parameters
+        ----------
+        ns: densities (na, nb)
+        d: order of derivative
+        """
+
+    def get_C(ns, d=0):
+        """
+        Parameters
+        ----------
+        ns: densities (na, nb)
+        d: order of derivative
+        """
+
+    def get_D(ns, d=0):
+        """
+        Parameters
+        ----------
+        ns: densities (na, nb)
+        d: order of derivative
+        """
+
+    def get_beta(ns, d=0):
+        """
+        Parameters
+        ----------
+        ns: densities (na, nb)
+        d: order of derivative
+        """
+
+    def get_Vs(**kw):
+        """
+        return the Vs(effective potential)
+        """
