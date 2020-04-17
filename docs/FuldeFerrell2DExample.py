009f692272f3e52daab48bc8d4ea878f507428eb
Chunde <xmuhcd@msn.com>
2020-04-17 10:28:38 -0700

    DOC; Update the FuldeFerrell example doc.

--- docs/FuldeFerrell2DExample.py
+++ docs/FuldeFerrell2DExample.py
@@ -14,7 +14,8 @@
 # ---
 
 # # Fulde Ferrell State Playgound
-# This notebook contain a solution to the FF State that has higher pressure than the superfluid state. Since we have two method to implement the FF State class, one is using the ClassFactory method, which can be extented to other functionals, but the code struture is a bit hard to understand. Another method is simply use the old FuldeFerrell Stete Class. I suggest to use the FuldeFerrellState class.
+# This notebook contain a solution to the FF State that has higher pressure than the superfluid state. Since we have two methods to implement the FF State class, one is using the ClassFactory method, which can be extented to other functionals, but the code struture is a bit hard to understand. Another method is simply use the old FuldeFerrell Stete Class. I suggest to use the FuldeFerrellState class.
+# The found FF State has fixed $g$ or $C$ using ($\Delta_0$=0.5, and $\mu=10$, $\delta \mu=0.446$, note: when compute $g$ or $C$, I only use $\Delta_0$ and $\mu$), then I found a FF state at ($dq= 0.11176852950565815, delta=0.05368101205886646$)
 
 import mmf_setup;mmf_setup.nbinit()
 # %pylab inline --no-import-all
@@ -38,8 +39,54 @@ currentdir = os.path.dirname(
 sys.path.insert(0, join(currentdir, '..','Projects','FuldeFerrellState'))
 from phase_diagram_generator import FFStateAgent
 
+# # Use the FF State Class
+
+from fulde_ferrell_state import FFState
+mu_eff = 10
+dmu_eff = 0.446
+delta0 = 0.5
+dim = 2
+k_c = 150
+ff = FFState(mu=mu_eff, dmu=dmu_eff, delta=delta0, dim=2, k_c=k_c, fix_g=True)
+
+# ## check if the found state satisifies the gap equation
+
+dq = 0.11176852950565815
+delta = 0.05368101205886646
+ff.f(mu=mu_eff, dmu=dmu_eff,dq=dq, delta=delta)
+
+# ## Check densites
+# * $n_a\ne n_b$
+
+ff.get_densities(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq)
+
+# ## FF State pressure and currents
+
+ff.get_pressure(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq, use_kappa=False)
+
+# * Current format: $j_a, j_b, j_+, j_-$, we may have a sign different for $j_b$, if filp, we will get total current eqaual to zero. I may be wrong.
+
+ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq)
+
+# ### Superfluid pressure
+
+ff.get_pressure(mu=mu_eff, dmu=dmu_eff, delta=delta0, use_kappa=False)
+
+# * current are all zero for superfluid state
+
+ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=delta0)
+
+# ### Normal state pressure
+
+ff.get_pressure(mu=mu_eff, dmu=dmu_eff, delta=0)
+
+ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=0)
+
+# ## Dicussion
+# The normal pressure is higher than superfluid pressure, they are just two different solutions. The Fulde Ferrell State has higher pressure than superful
+
 # # Use the ClassFactory Method
-# * To use the FuldeFerrellState class, see the next section
+# * To use the FuldeFerrellState class, see the previous section
 
 mu_eff = 10
 dmu_eff = 0.446
@@ -73,8 +120,6 @@ delta = 0.05368101205886646
 
 f(delta=delta, dq=dq)
 
-f(delta=delta, dq=dq)
-
 # ## FF State Pressure
 
 p_ff=lda.get_ns_mus_e_p(mus_eff=mus_eff, delta=delta, dq=dq)[3]
@@ -98,43 +143,5 @@ assert p_ff > p_sf
 ja, jb, jp, jm = lda.get_current(mus_eff=mus_eff, delta=delta, dq=dq)
 
 print(ja, jb, jp, jm)
-
-# # Use the FF State Class
-
-from fulde_ferrell_state import FFState
-mu_eff = 10
-dmu_eff = 0.446
-delta0 = 0.5
-dim = 2
-k_c = 150
-ff = FFState(mu=mu_eff, dmu=dmu_eff, delta=delta0, dim=2, k_c=k_c, fix_g=True)
-
-# ## check if the found state satisifies the gap equation
-
-dq = 0.11176852950565815
-delta = 0.05368101205886646
-ff.f(mu=mu_eff, dmu=dmu_eff,dq=dq, delta=delta)
-
-# ### FF State pressure and currents
-
-ff.get_pressure(mu_eff=mu_eff, dmu_eff=dmu_eff, delta=delta, dq=dq, use_kappa=False)
-
-# * Current format: $j_a, j_b, j_+, j_-$, we may have a sign different for $j_b$, if filp, we will get total current eqaual to zero. I may be wrong.
-
-ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=delta, dq=dq)
-
-# ### Superfluid pressure
-
-ff.get_pressure(mu_eff=mu_eff, dmu_eff=dmu_eff, delta=delta0, use_kappa=False)
-
-# * current are all zero for superfluid state
-
-ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=delta0)
-
-# ### Normal state pressure
-
-ff.get_pressure(mu_eff=mu_eff, dmu_eff=dmu_eff, delta=0, use_kappa=False)
-
-ff.get_current(mu=mu_eff, dmu=dmu_eff, delta=0)
 
 
